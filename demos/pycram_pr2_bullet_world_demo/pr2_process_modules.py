5d37859fed30eb486d5af98255e04a117b7811b4
Thomas Lipps <lipps.thomas@web.de>
2020-11-19 15:45:23 +0100

    [PMs] Renamed file with pr2 PMs from to pr2_process_modules

--- demos/pycram_pr2_bullet_world_demo/pr2_process_modules.py
+++ demos/pycram_pr2_bullet_world_demo/pr2_process_modules.py
@@ -1,5 +1,6 @@
 from pycram.robot_description import InitializedRobotDescription as robot_description
 from pycram.process_module import ProcessModule
+from pycram.process_modules import ProcessModules
 from pycram.bullet_world import BulletWorld
 from pycram.helper import transform
 import pycram.bullet_world_reasoning as btr
@@ -25,7 +26,7 @@ def _park_arms(arm):
     defined robot.
     :return:
     """
-    #joint_poses = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.9, -0.1, 1.6, 1.7,
+    # joint_poses = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.9, -0.1, 1.6, 1.7,
     #               0.087, 1.2, -1.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.9, -0.1, 1.6,
     #               -1.7, -0.08, -1.2, 1.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
@@ -42,17 +43,20 @@ class Pr2Navigation(ProcessModule):
     """
     The process module to move the robot from one position to another.
     """
+
     def _execute(self, desig):
         solution = desig.reference()
         if solution['cmd'] == 'navigate':
             robot = BulletWorld.robot
             robot.set_position_and_orientation(solution['target'], solution['orientation'])
 
+
 class Pr2PickUp(ProcessModule):
     """
     This process module is for picking up a given object.
     The object has to be reachable for this process module to succeed.
     """
+
     def _execute(self, desig):
         solution = desig.reference()
         if solution['cmd'] == 'pick':
@@ -70,13 +74,14 @@ class Pr2Place(ProcessModule):
     """
     This process module places an object at the given position in world coordinate frame.
     """
+
     def _execute(self, desig):
         solution = desig.reference()
         if solution['cmd'] == 'place':
             object = solution['object']
             robot = BulletWorld.robot
             inv = p.calculateInverseKinematics(robot.id, robot.get_link_id(solution['gripper']), solution['target'],
-                                           maxNumIterations=100)
+                                               maxNumIterations=100)
             _apply_ik(robot, inv)
             robot.detach(object)
             time.sleep(0.5)
@@ -89,6 +94,7 @@ class Pr2Accessing(ProcessModule):
     This provides the illusion the robot would open the drawer by himself.
     Then the drawer will be opened by setting the joint pose of the drawer joint.
     """
+
     def _execute(self, desig):
         solution = desig.reference()
         if solution['cmd'] == 'access':
@@ -98,7 +104,8 @@ class Pr2Accessing(ProcessModule):
             drawer_handle = solution['drawer-handle']
             drawer_joint = solution['drawer-joint']
             dis = solution['distance']
-            inv = p.calculateInverseKinematics(robot.id, robot.get_link_id(gripper), kitchen.get_link_position(drawer_handle))
+            inv = p.calculateInverseKinematics(robot.id, robot.get_link_id(gripper),
+                                               kitchen.get_link_position(drawer_handle))
             _apply_ik(robot, inv)
             time.sleep(0.2)
             han_pose = kitchen.get_link_position(drawer_handle)
@@ -115,6 +122,7 @@ class Pr2ParkArms(ProcessModule):
     This process module is for moving the arms in a parking position.
     It is currently not used.
     """
+
     def _execute(self, desig):
         solutions = desig.reference()
         if solutions['cmd'] == 'park':
@@ -126,6 +134,7 @@ class Pr2MoveHead(ProcessModule):
     This process module moves the head to look at a specific point in the world coordinate frame.
     This point can either be a position or an object.
     """
+
     def _execute(self, desig):
         solutions = desig.reference()
         if solutions['cmd'] == 'looking':
@@ -135,7 +144,7 @@ class Pr2MoveHead(ProcessModule):
             pose_in_tilt = transform(target, robot.get_link_position("head_tilt_link"))
 
             new_pan = np.arctan([pose_in_pan[1], pose_in_pan[0]])
-            new_tilt = np.arctan([-pose_in_tilt[2], pose_in_tilt[0]**2 + pose_in_tilt[1]**2])
+            new_tilt = np.arctan([-pose_in_tilt[2], pose_in_tilt[0] ** 2 + pose_in_tilt[1] ** 2])
 
             robot.set_joint_state("head_pan_joint", new_pan[0])
             robot.set_joint_state("head_tilt_joint", new_tilt[0])
@@ -146,6 +155,7 @@ class Pr2MoveGripper(ProcessModule):
     This process module controls the gripper of the robot. They can either be opened or closed.
     Furthermore, it can only moved one gripper at a time.
     """
+
     def _execute(self, desig):
         solution = desig.reference()
         if solution['cmd'] == "move-gripper":
@@ -163,6 +173,7 @@ class Pr2Detecting(ProcessModule):
     This process module tries to detect an object with the given type. To be detected the object has to be in
     the field of view of the robot.
     """
+
     def _execute(self, desig):
         solution = desig.reference()
         if solution['cmd'] == "detecting":
@@ -181,6 +192,7 @@ class Pr2MoveTCP(ProcessModule):
     """
     This process moves the tool center point of either the right or the left arm.
     """
+
     def _execute(self, desig):
         solution = desig.reference()
         if solution['cmd'] == "move-tcp":
@@ -197,6 +209,7 @@ class Pr2MoveJoints(ProcessModule):
     This process modules moves the joints of either the right or the left arm. The joint states can be given as
     list that should be applied or a pre-defined position can be used, such as "parking"
     """
+
     def _execute(self, desig):
         solution = desig.reference()
         if solution['cmd'] == "move-joints":
@@ -222,6 +235,7 @@ class Pr2WorldStateDetecting(ProcessModule):
     """
     This process module detectes an object even if it is not in the field of view of the robot.
     """
+
     def _execute(self, desig):
         solution = desig.reference()
         if solution['cmd'] == "world-state-detecting":
@@ -229,60 +243,12 @@ class Pr2WorldStateDetecting(ProcessModule):
             return list(filter(lambda obj: obj.type == obj_type, BulletWorld.current_bullet_world.objects))[0]
 
 
-pr2_navigation = Pr2Navigation()
-pr2_pick_up = Pr2PickUp()
-pr2_place = Pr2Place()
-pr2_accessing = Pr2Accessing()
-pr2_park_arms = Pr2ParkArms()
-pr2_move_head = Pr2MoveHead()
-pr2_move_gripper = Pr2MoveGripper()
-pr2_detecting = Pr2Detecting()
-pr2_move_tcp = Pr2MoveTCP()
-pr2_move_joints = Pr2MoveJoints()
-pr2_world_state_detecting = Pr2WorldStateDetecting()
+class PR2ProcessModules(ProcessModules):
+    initialized = None
 
-
-def available_process_modules(desig):
-    """
-    This method chooses the right process module for the given designator and returns it.
-    :param desig: The designator for which a process module should be choosen.
-    :return: The choosen process module
-    """
-    if desig.check_constraints([('type', 'moving')]):
-        return pr2_navigation
-
-    if desig.check_constraints([('type', 'pick-up')]):
-        return pr2_pick_up
-
-    if desig.check_constraints([('type', 'place')]):
-        return pr2_place
-
-    if desig.check_constraints([('type', 'accessing')]):
-        return pr2_accessing
-
-    if desig.check_constraints([('type', 'park-arms')]):
-        return pr2_park_arms
-
-    if desig.check_constraints([('type', 'looking')]):
-        return pr2_move_head
-
-    if desig.check_constraints([('type', 'opening-gripper')]):
-        return pr2_move_gripper
-
-    if desig.check_constraints([('type', 'closing-gripper')]):
-        return pr2_move_gripper
-
-    if desig.check_constraints([('type', 'detecting')]):
-        return pr2_detecting
-
-    if desig.check_constraints([('type', 'move-tcp')]):
-        return pr2_move_tcp
-
-    if desig.check_constraints([('type', 'move-arm-joints')]):
-        return pr2_move_joints
-
-    if desig.check_constraints([('type', 'world-state-detecting')]):
-        return pr2_world_state_detecting
-
-
-ProcessModule.resolvers.append(available_process_modules)
+    def __init__(self):
+        if not PR2ProcessModules.initialized:
+            super().__init__(Pr2Navigation(), Pr2PickUp(), Pr2Place(), Pr2Accessing(), Pr2ParkArms(), Pr2MoveHead(),
+                             Pr2MoveGripper(), Pr2MoveGripper(), Pr2Detecting(), Pr2MoveTCP(), Pr2MoveJoints(),
+                             Pr2WorldStateDetecting())
+            PR2ProcessModules.initialized = self
