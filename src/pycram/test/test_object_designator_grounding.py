d02b72c535c5a6bef5c05fe8b4024821f3230b57
Benjamin Alt <benjamin_alt@outlook.com>
2021-09-28 13:21:34 +0200

    Small improvements for grounding designators via KnowRob

--- src/pycram/test/test_object_designator_grounding.py
+++ src/pycram/test/test_object_designator_grounding.py
@@ -21,11 +21,21 @@ class TestObjectDesignatorGrounding(TestCase):
         knowrob.clear_beliefstate()
 
     def test_ground_located_object_by_type(self):
+        """
+        Test grounding of ObjectDesignator when just the type is given.
+        Expected behavior: Repeated calls to desig.next_solution() successively yield groundings for all known objects
+        of that type
+        """
         desc = LocatedObjectDesignatorDescription(type_="a")
         desig = ObjectDesignator(desc)
         all_solutions = self._get_all_solutions(desig)
         names = [sol["name"] for sol in all_solutions]
         self.assertSetEqual(set(names), {"a1", "a2"})
+        for sol in all_solutions:
+            if sol["name"] == "a1":
+                self.assertEqual(sol["pose"], [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0])
+            elif sol["name"] == "a2":
+                self.assertEqual(sol["pose"], [2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 1.0])
 
     @staticmethod
     def _get_all_solutions(desig):
