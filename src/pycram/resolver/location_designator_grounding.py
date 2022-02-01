99b267d0b392683df5d97dc02aca978eeb0b9d79
Jonas Dech <j.dech@gmx.net>
2022-02-01 13:04:19 +0100

    [resolver/location] reworked gen_from_costmap

--- src/pycram/resolver/location_designator_grounding.py
+++ src/pycram/resolver/location_designator_grounding.py
@@ -2,7 +2,7 @@ from pycram.designator import DesignatorError
 from pycram.helper import transform
 from pycram.location_designator import ObjectRelativeLocationDesignatorDescription, LocationDesignator, \
     LocationDesignatorDescription
-from pycram.costmaps import GaussianCostmap, OccupancyMap, VisibilityCostmap
+from pycram.costmaps import GaussianCostmap, OccupancyCostmap, VisibilityCostmap
 from pycram.robot_description import InitializedRobotDescription as robot_description
 from pycram.bullet_world import BulletWorld
 from pycram.pose_generator_and_validator import pose_generator, visibility_validator, reachability_validator
@@ -36,7 +36,7 @@ def call_ground(desig):
 def gen_from_costmap(desig):
     min_height = list(robot_description.i.cameras.values())[0].min_height
     max_height = list(robot_description.i.cameras.values())[0].max_height
-    occupancy = OccupancyMap(0.2, False, 200, 0.02, desig._description.pose, BulletWorld.current_bullet_world)
+    occupancy = OccupancyCostmap(0., False, 200, 0.02, desig._description.pose, BulletWorld.current_bullet_world)
     final_map = occupancy
     if desig._description.reachable_for:
         gaussian = GaussianCostmap(200, 15, 0.02, desig._description.pose)
@@ -46,18 +46,20 @@ def gen_from_costmap(desig):
         final_map += visible
 
     test_world = BulletWorld.current_bullet_world.copy()
-    test_robot = test_world.get_objects_by_name(desig._description.visible_for.name)[0]
+    robot_object = desig._description.visible_for if desig._description.visible_for else desig._description.reachable_for
+    test_robot = test_world.get_objects_by_name(robot_object.name)[0]
 
     for maybe_pose in pose_generator(final_map):
         res = True
         if desig._description.visible_for:
             res = res and visibility_validator(maybe_pose, test_robot, desig._description.pose, test_world)
         if desig._description.reachable_for:
-            res = res and reachability_validator(maybe_pose, test_robot, desig._description.pose, robot_description.i.get_tool_frame('left'), test_world)
+            res = res and reachability_validator(maybe_pose, test_robot, desig._description.pose, test_world)
 
         if res:
-            print(maybe_pose)
+            #print(maybe_pose)
             yield {'position': maybe_pose[0], 'orientation': maybe_pose[1]}
+    test_world.exit()
 
 
 LocationDesignator.resolvers['grounding'] = call_ground
