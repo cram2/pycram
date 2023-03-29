19f34666cf96a21b2e83db6ac3c5e99dd8754af1
Jonas Dech <j.dech@gmx.net>
2023-03-29 16:49:50 +0200

    removed wrongly commited files

--- om_thread_test.py
+++
@@ -1,8 +0,0 @@
-from pycram.bullet_world import BulletWorld, Object
-import pycram.costmaps as c
-import time
-
-world = BulletWorld()
-kitchen = Object("Kitchen", "env", "kitchen.urdf")
-
-o = c.OccupancyCostmap(0.2, False, 200, 0.2)
--- typing_test.py
+++
@@ -1,6 +0,0 @@
-from pycram.language import macros, seq, par
-
-exceptions = []
-with seq(exceptions) as state:
-    print("typing")
-    a = 1
