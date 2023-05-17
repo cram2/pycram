696fa8904ec8505db47a69a73d8f220413709960
Tom Schierenbeck <tom_sch@uni-bremen.de>
2023-05-17 12:17:08 +0200

    [Costmaps] Started integrating Occupancy costmaps in JPTCostmaps.

--- src/pycram/resolver/location/jpt_location.py
+++ src/pycram/resolver/location/jpt_location.py
@@ -1,31 +1,19 @@
 import dataclasses
-import itertools
+import time
 from typing import Optional, List, Tuple
 
 import jpt
 import numpy as np
-import pandas as pd
-import plotly.graph_objects as go
 import pybullet
-import sqlalchemy.orm
 import tf
-import tqdm
 
-import pycram.costmaps
 import pycram.designators.location_designator
 import pycram.task
-from pycram.bullet_world import BulletWorld, Object as BulletWorldObject
-from pycram.designators.action_designator import MoveTorsoAction, PickUpAction, NavigateAction
-from pycram.designators.object_designator import ObjectDesignatorDescription
+from pycram.costmaps import OccupancyCostmap, plot_grid
 from pycram.plan_failures import PlanFailure
-from pycram.process_module import simulated_robot
-from pycram.costmaps import OccupancyCostmap
-from sqlalchemy.ext.compiler import compiles
-import mlflow
 
 
 class JPTCostmapLocation(pycram.designators.location_designator.CostmapLocation):
-
     @dataclasses.dataclass
     class Location(pycram.designators.location_designator.LocationDesignatorDescription.Location):
         pose: Tuple[List[float], List[float]]
@@ -48,7 +36,35 @@ class JPTCostmapLocation(pycram.designators.location_designator.CostmapLocation)
 
         self.visual_ids: List[int] = []
 
+    def evidence_from_occupancy_costmap(self):
+        ocm = OccupancyCostmap(distance_to_obstacle=0.4, from_ros=False, size=200, resolution=0.02,
+                               origin=self.target.get_position_and_orientation())
+        ocm.visualize()
+
+        # working on a copy of the costmap, since found rectangles are deleted
+        map = np.copy(ocm.map)
+        curr_width = 0
+        curr_height = 0
+        curr_pose = []
+        boxes = []
+
+        for i in range(0, map.shape[0]):
+            for j in range(0, map.shape[1]):
+                if map[i][j] > 0:
+                    curr_width = ocm._find_consectuive_line((i, j), map)
+                    curr_pose = (i, j)
+                    curr_height = ocm._find_max_box_height((i,j), curr_width, map)
+                    avg = np.average(map[i:i+curr_height, j:j+curr_width])
+                    boxes.append([curr_pose, curr_height, curr_width, avg])
+                    map[i:i+curr_height, j:j+curr_width] = 0
+        print(boxes)
+        exit()
+
+
     def create_evidence(self, use_success=True):
+
+        print("AMINA")
+        self.evidence_from_occupancy_costmap()
         evidence = dict()
 
         if self.reachable_for:
@@ -63,10 +79,6 @@ class JPTCostmapLocation(pycram.designators.location_designator.CostmapLocation)
 
     def sample(self, amount: int = 1):
         """Sample from the locations that fit the CostMap."""
-
-        # cm = OccupancyCostmap(distance_to_obstacle=0.4, from_ros=False, size=100, resolution=0.02,
-        #                       origin=self.target.pose)
-
         evidence = self.create_evidence()
 
         try:
@@ -82,14 +94,13 @@ class JPTCostmapLocation(pycram.designators.location_designator.CostmapLocation)
 
     def sample_to_location(self, sample: np.ndarray) -> Location:
         sample_dict = {variable.name: value for variable, value in zip(self.model.variables, sample)}
-
-        angle = np.arctan2(sample_dict["y_position"] - self.target.pose[1], sample_dict["x_position"] -
-                           self.target.pose[0]) + np.pi
-
         target_x, target_y, target_z = self.target.pose
-        pose = [target_x - sample_dict["x_position"], target_y - sample_dict["y_position"], 0]
+        pose = [target_x + sample_dict["x_position"], target_y + sample_dict["y_position"], 0]
+
+        angle = np.arctan2(pose[1] - target_y, pose[0] - target_x) + np.pi
+
         orientation = list(tf.transformations.quaternion_from_euler(0, 0, angle, axes="sxyz"))
-        torso_height = target_z - sample_dict["z_position"]
+        torso_height = target_z - sample_dict["torso_height"]
         result = self.Location((pose, orientation), sample_dict["arm"], torso_height, sample_dict["grasp"])
         return result
 
@@ -128,8 +139,7 @@ class JPTCostmapLocation(pycram.designators.location_designator.CostmapLocation)
 
             self.visual_ids.append(visual)
 
-        for id_list in np.array_split(np.array(self.visual_ids), np.ceil(len(self.visual_ids)/127)):
-
+        for id_list in np.array_split(np.array(self.visual_ids), np.ceil(len(self.visual_ids) / 127)):
             # Dummy paramater since these are needed to spawn visual shapes as a multibody.
             link_poses = [[0, 0, 0] for c in id_list]
             link_orientations = [[0, 0, 0, 1] for c in id_list]
--- test/test_jpt_resolver.py
+++ test/test_jpt_resolver.py
@@ -1,8 +1,10 @@
 import os
-import time
 import unittest
+import time
 
 import jpt
+import numpy as np
+import requests
 
 from pycram.bullet_world import BulletWorld, Object
 from pycram.designators import action_designator, object_designator
@@ -11,7 +13,17 @@ from pycram.process_module import simulated_robot
 from pycram.resolver.location.jpt_location import JPTCostmapLocation
 from pycram.robot_descriptions.robot_description_handler import InitializedRobotDescription as robot_description
 
+mlflow_uri = os.getenv('MLFLOW_TRACKING_URI')
+if mlflow_uri is None:
+    mlflow_uri = "http://127.0.0.1:5000"
 
+try:
+    response = requests.get(mlflow_uri, timeout=2)
+except requests.exceptions.ConnectionError:
+    response = None
+
+
+@unittest.skipIf(response is None or (response.status_code != requests.codes.ok), "mlflow server is not available.")
 class JPTResolverTestCase(unittest.TestCase):
     world: BulletWorld
     milk: Object
@@ -20,26 +32,42 @@ class JPTResolverTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
+        np.random.seed(420)
         cls.model = jpt.JPT.load(os.path.join(os.path.expanduser("~"), "Documents", "grasping.jpt"))
         cls.world = BulletWorld("GUI")
         cls.milk = Object("milk", "milk", "milk.stl", position=[3, 3, 0.75])
         cls.robot = Object(robot_description.i.name, "pr2", robot_description.i.name + ".urdf")
         ProcessModule.execution_delay = False
 
-    def test_costmap(self):
+    def test_costmap_no_obstacles(self):
         cml = JPTCostmapLocation(self.milk, reachable_for=self.robot, model=self.model)
         sample = next(iter(cml))
-        print(sample)
-        cml.visualize()
 
         with simulated_robot:
             action_designator.NavigateAction.Action(sample.pose).perform()
             action_designator.MoveTorsoAction.Action(sample.torso_height).perform()
-            time.sleep(5)
             action_designator.PickUpAction.Action(
                 object_designator.ObjectDesignatorDescription(types=["milk"]).resolve(),
                 arm=sample.reachable_arm, grasp=sample.grasp).perform()
-            time.sleep(5)
+
+    def test_costmap_with_obstacles(self):
+        kitchen = Object("kitchen", "environment", "kitchen.urdf")
+        self.milk.set_position([-1.2, 1, 0.98])
+        cml = JPTCostmapLocation(self.milk, reachable_for=self.robot, model=self.model)
+        sample = next(iter(cml))
+
+
+
+        with simulated_robot:
+            action_designator.NavigateAction.Action(sample.pose).perform()
+            action_designator.MoveTorsoAction.Action(sample.torso_height).perform()
+            action_designator.PickUpAction.Action(
+                object_designator.ObjectDesignatorDescription(types=["milk"]).resolve(),
+                arm=sample.reachable_arm, grasp=sample.grasp).perform()
+
+    def tearDown(self) -> None:
+        self.world.reset_bullet_world()
+
     @classmethod
     def tearDownClass(cls) -> None:
         cls.world.exit()
