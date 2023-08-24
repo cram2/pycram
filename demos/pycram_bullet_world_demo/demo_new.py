6f2ca9899492c7653b56628fcaff469710663503
Jonas Dech <j.dech@gmx.net>
2023-08-24 16:24:53 +0200

    [demos] finished version of new demo plan

--- demos/pycram_bullet_world_demo/demo_new.py
+++ demos/pycram_bullet_world_demo/demo_new.py
@@ -6,19 +6,19 @@ from pycram.bullet_world import BulletWorld, Object
 from pycram.process_module import simulated_robot, with_simulated_robot
 
 world = BulletWorld()
-robot = Object("pr2", "robot", "pr2.urdf", pose=Pose([2, 1, 0]))
+robot = Object("pr2", "robot", "pr2.urdf", pose=Pose([1, 2, 0]))
 apartment = Object("apartment", "environment", "apartment.urdf")
 
-milk = Object("milk", "milk", "milk.stl", pose=Pose([3.05, 2, 1.02], [0, 0, 1, 0]))
-cereal = Object("cereal", "cereal", "breakfast_cereal.stl", pose=Pose([3.05, 2.3, 1.05], [0, 0, 1, 0]))
-spoon = Object("spoon", "spoon", "spoon.stl", pose=Pose([2.2, 2, 0.85], [0, 0, 0, 1]))
-bowl = Object("bowl", "bowl", "bowl.stl", pose=Pose([3.05, 2.2, 1.02], [0, 0, 1, 0]))
+milk = Object("milk", "milk", "milk.stl", pose=Pose([2.5, 2, 1.02]))
+cereal = Object("cereal", "cereal", "breakfast_cereal.stl", pose=Pose([2.5, 2.3, 1.05]))
+spoon = Object("spoon", "spoon", "spoon.stl", pose=Pose([2.4, 2.2, 0.85]))
+bowl = Object("bowl", "bowl", "bowl.stl", pose=Pose([2.5, 2.2, 1.02]))
 apartment.attach(spoon, 'cabinet10_drawer_top')
 
 robot_desig = BelieveObject(names=["pr2"])
 apartment_desig = BelieveObject(names=["apartment"])
-milk_desig = BelieveObject(names=["cereal"])
-cereal_desig = BelieveObject(names=["milk"])
+milk_desig = BelieveObject(names=["milk"])
+cereal_desig = BelieveObject(names=["cereal"])
 spoon_desig = BelieveObject(names=["spoon"])
 bowl_desig = BelieveObject(names=["bowl"])
 
@@ -26,24 +26,36 @@ bowl_desig = BelieveObject(names=["bowl"])
 with simulated_robot:
     MoveTorsoAction([0.25]).resolve().perform()
 
-    TransportAction(milk_desig, ["left"], [Pose([5, 3.8, 1])]).resolve().perform()
+    TransportAction(milk_desig, ["left"], [Pose([4.8, 3.55, 0.8])]).resolve().perform()
 
-    TransportAction(cereal_desig, ["right"], [Pose([4.9, 3.7, 1])]).resolve().perform()
+    TransportAction(cereal_desig, ["right"], [Pose([5.2, 3.4, 0.8], [0, 0, 1, 1])]).resolve().perform()
 
-    TransportAction(bowl_desig, ["left"], [Pose([5, 4, 1])]).resolve().perform()
+    TransportAction(bowl_desig, ["left"], [Pose([5, 3.3, 0.8], [0, 0, 1, 1])], ["top"]).resolve().perform()
 
-    handle_desig = ObjectPart(names=["cabinet10_drawer_top"], part_of=apartment_desig.resolve()).resolve()
-    drawer_open_loc = AccessingLocation(handle_desig=handle_desig, robot_desig=robot_desig.resolve()).resolve()
+    handle_desig = ObjectPart(names=["handle_cab10_t"], part_of=apartment_desig.resolve())
+    drawer_open_loc = AccessingLocation(handle_desig=handle_desig.resolve(), robot_desig=robot_desig.resolve()).resolve()
 
-    NavigateAction([drawer_open_loc]).resolve().perform()
+    NavigateAction([drawer_open_loc.pose]).resolve().perform()
 
     OpenAction(object_designator_description=handle_desig, arms=[drawer_open_loc.arms[0]]).resolve().perform()
+    spoon.detach(apartment)
 
-    PickUpAction(spoon_desig, ["right"], ["top"]).resolve().perform()
+    PickUpAction(spoon_desig, ["left"], ["top"]).resolve().perform()
 
-    placing_loc = CostmapLocation(target=Pose([5.1, 4, 1], [0, 0, 1, 1])).resolve()
+    ParkArmsAction([Arms.BOTH]).resolve().perform()
 
-    NavigateAction([placing_loc]).resolve().perform()
+    CloseAction(object_designator_description=handle_desig, arms=[drawer_open_loc.arms[0]]).resolve().perform()
 
-    PlaceAction(spoon_desig, [Pose([5.1, 4, 1]), [0, 0, 1, 1]], ["right"]).resolve().perform()
+    ParkArmsAction([Arms.BOTH]).resolve().perform()
+
+    MoveTorsoAction([0.15]).resolve().perform()
+
+    spoon_target_pose = Pose([4.85, 3.3, 0.8], [0, 0, 1, 1])
+    placing_loc = CostmapLocation(target=spoon_target_pose, reachable_for=robot_desig.resolve()).resolve()
+
+    NavigateAction([placing_loc.pose]).resolve().perform()
+
+    PlaceAction(spoon_desig, [spoon_target_pose], ["left"]).resolve().perform()
+
+    ParkArmsAction([Arms.BOTH]).resolve().perform()
 
