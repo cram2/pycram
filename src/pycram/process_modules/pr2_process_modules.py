4cfaa14ddba4d995fc67ef987c787eb544160bfa
Jonas Dech <j.dech@gmx.net>
2023-08-31 15:34:57 +0200

    [pr2 pms] Added first implementation of real pms

--- src/pycram/process_modules/pr2_process_modules.py
+++ src/pycram/process_modules/pr2_process_modules.py
@@ -1,9 +1,11 @@
 from abc import ABC
 from threading import Lock
+from typing import Any
 
 import pycram.bullet_world_reasoning as btr
 import numpy as np
 import time
+import rospy
 import pybullet as p
 
 from ..plan_failures import EnvironmentManipulationImpossible
@@ -16,6 +18,7 @@ from ..helper import _transform_to_torso, _apply_ik, calculate_wrist_tool_offset
 from ..local_transformer import LocalTransformer
 from ..designators.motion_designator import *
 from ..enums import JointType
+from ..external_interfaces import giskard
 
 
 def _park_arms(arm):
@@ -215,6 +218,7 @@ class Pr2Close(ProcessModule):
     """
     Low-level implementation that lets the robot close a grasped container, in simulation
     """
+
     def _execute(self, desig: ClosingMotion.Motion):
         part_of_object = desig.object_part.bullet_world_object
 
@@ -239,6 +243,125 @@ def _move_arm_tcp(target: Pose, robot: Object, arm: str) -> None:
     _apply_ik(robot, inv, joints)
 
 
+###########################################################
+########## Process Modules for the Real PR2 ###############
+###########################################################
+
+
+class Pr2NavigationReal(ProcessModule):
+    """
+    Process module for the real PR2 that sends a cartesian goal to giskard to move the robot base
+    """
+    def _execute(self, designator: MoveMotion.Motion) -> Any:
+        rospy.logdebug(f"Sending goal to giskard to Move the robot")
+        giskard.achieve_cartesian_goal(designator.target, robot_description.base_link, "map")
+
+
+class Pr2PickUpReal(ProcessModule):
+
+    def _execute(self, designator: PickUpMotion.Motion) -> Any:
+        pass
+
+
+class Pr2PlaceReal(ProcessModule):
+
+    def _execute(self, designator: MotionDesignatorDescription.Motion) -> Any:
+        pass
+
+
+class Pr2MoveHeadReal(ProcessModule):
+    """
+    Process module for the real robot to move that such that it looks at the given position. Uses the same calculation
+    as the simulated one
+    """
+    def _execute(self, desig: LookingMotion.Motion):
+        target = desig.target
+        robot = BulletWorld.robot
+
+        local_transformer = LocalTransformer()
+        pose_in_pan = local_transformer.transform_pose(target, robot.get_link_tf_frame("head_pan_link"))
+        pose_in_tilt = local_transformer.transform_pose(target, robot.get_link_tf_frame("head_tilt_link"))
+
+        new_pan = np.arctan2(pose_in_pan.position.y, pose_in_pan.position.x)
+        new_tilt = np.arctan2(pose_in_tilt.position.z, pose_in_tilt.position.x ** 2 + pose_in_tilt.position.y ** 2) * -1
+
+        current_pan = robot.get_joint_state("head_pan_joint")
+        current_tilt = robot.get_joint_state("head_tilt_joint")
+
+        giskard.avoid_all_collisions()
+        giskard.achieve_joint_goal({"head_pan_joint": new_pan + current_pan,
+                                    "head_tilt_joint": new_tilt + current_tilt})
+
+
+class Pr2DetectingReal(ProcessModule):
+    """
+    Process Module for the real Pr2 that tries to detect an object fitting the given object description. Uses Robokudo
+    for perception of the environment.
+    """
+    def _execute(self, designator: DetectingMotion.Motion) -> Any:
+        pass
+
+
+class Pr2MoveTCPReal(ProcessModule):
+    """
+    Moves the tool center point of the real PR2 while avoiding all collisions
+    """
+    def _execute(self, designator: MoveTCPMotion.Motion) -> Any:
+        lt = LocalTransformer()
+        pose_in_map = lt.transform_pose(designator.target, "map")
+
+        giskard.avoid_all_collisions()
+        giskard.achieve_cartesian_goal(pose_in_map, robot_description.get_tool_frame(designator.arm),
+                                       robot_description.base_link)
+
+
+class Pr2MoveArmJointsReal(ProcessModule):
+    """
+    Moves the arm joints of the real PR2 to the given configuration while avoiding all collisions
+    """
+    def _execute(self, designator: MoveArmJointsMotion.Motion) -> Any:
+        giskard.avoid_all_collisions()
+        if designator.left_arm_poses:
+            giskard.achieve_joint_goal(designator.left_arm_poses)
+
+        if designator.right_arm_poses:
+            giskard.achieve_joint_goal(designator.right_arm_poses)
+
+
+class Pr2MoveJointsReal(ProcessModule):
+    """
+    Moves any joint using giskard, avoids all collisions while doint this.
+    """
+    def _execute(self, designator: MoveJointsMotion.Motion) -> Any:
+        name_to_position = zip(designator.names, designator.positions)
+        giskard.avoid_all_collisions()
+        giskard.achieve_joint_goal(name_to_position)
+
+
+class Pr2MoveGripperReal(ProcessModule):
+
+    def _execute(self, designator: MoveGripperMotion.Motion) -> Any:
+        pass
+
+
+class Pr2OpenReal(ProcessModule):
+    """
+    Tries to open an already grasped container
+    """
+    def _execute(self, designator: OpeningMotion.Motion) -> Any:
+        giskard.achieve_open_container_goal(robot_description.get_tool_frame(designator.arm),
+                                            designator.object_part.name)
+
+
+class Pr2CloseReal(ProcessModule):
+    """
+    Tries to close an already grasped container
+    """
+    def _execute(self, designator: ClosingMotion.Motion) -> Any:
+        giskard.achieve_close_container_goal(robot_description.get_tool_frame(designator.arm),
+                                             designator.object_part.name)
+
+
 class Pr2Manager(ProcessModuleManager):
 
     def __init__(self):
@@ -259,18 +382,26 @@ class Pr2Manager(ProcessModuleManager):
     def navigate(self):
         if ProcessModuleManager.execution_type == "simulated":
             return Pr2Navigation(self._navigate_lock)
+        elif ProcessModuleManager.execution_type == "real":
+            return Pr2NavigationReal(self._navigate_lock)
 
     def pick_up(self):
         if ProcessModuleManager.execution_type == "simulated":
             return Pr2PickUp(self._pick_up_lock)
+        elif ProcessModuleManager.execution_type == "real":
+            return Pr2PickUpReal(self._pick_up_lock)
 
     def place(self):
         if ProcessModuleManager.execution_type == "simulated":
             return Pr2Place(self._place_lock)
+        elif ProcessModuleManager.execution_type == "real":
+            return Pr2PlaceReal(self._place_lock)
 
     def looking(self):
         if ProcessModuleManager.execution_type == "simulated":
             return Pr2MoveHead(self._looking_lock)
+        elif ProcessModuleManager.execution_type == "real":
+            return Pr2MoveHeadReal(self._looking_lock)
 
     def detecting(self):
         if ProcessModuleManager.execution_type == "simulated":
@@ -279,18 +410,24 @@ class Pr2Manager(ProcessModuleManager):
     def move_tcp(self):
         if ProcessModuleManager.execution_type == "simulated":
             return Pr2MoveTCP(self._move_tcp_lock)
+        elif ProcessModuleManager.execution_type == "real":
+            return Pr2MoveTCPReal(self._move_tcp_lock)
 
     def move_arm_joints(self):
         if ProcessModuleManager.execution_type == "simulated":
             return Pr2MoveArmJoints(self._move_arm_joints_lock)
+        elif ProcessModuleManager.execution_type == "real":
+            return Pr2MoveArmJointsReal(self._move_arm_joints_lock)
 
     def world_state_detecting(self):
-        if ProcessModuleManager.execution_type == "simulated":
+        if ProcessModuleManager.execution_type == "simulated" or ProcessModuleManager.execution_type == "real":
             return Pr2WorldStateDetecting(self._world_state_detecting_lock)
 
     def move_joints(self):
         if ProcessModuleManager.execution_type == "simulated":
             return PR2MoveJoints(self._move_joints_lock)
+        elif ProcessModuleManager.execution_type == "real":
+            return Pr2MoveJointsReal(self._move_joints_lock)
 
     def move_gripper(self):
         if ProcessModuleManager.execution_type == "simulated":
@@ -299,7 +436,11 @@ class Pr2Manager(ProcessModuleManager):
     def open(self):
         if ProcessModuleManager.execution_type == "simulated":
             return Pr2Open(self._open_lock)
+        elif ProcessModuleManager.execution_type == "real":
+            return Pr2OpenReal(self._open_lock)
 
     def close(self):
         if ProcessModuleManager.execution_type == "simulated":
             return Pr2Close(self._close_lock)
+        elif ProcessModuleManager.execution_type == "real":
+            return Pr2CloseReal(self._close_lock)
