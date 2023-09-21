bd78868b49978b757bbad967c75a92dea3343609
Nils Leusmann <leusmann@uni-bremen.de>
2023-09-21 09:37:57 +0200

    Refactors code to convey to the code conventions

--- src/pycram/orm/utils.py
+++ src/pycram/orm/utils.py
@@ -1,86 +1,98 @@
 import traceback
 
+import rospy
 import sqlalchemy
 import pycram.orm.base
 from pycram.designators.action_designator import *
 from pycram.designators.object_designator import *
 
 
-def logDB(in_session, filename, b_write_to_console=False):
+def write_database_to_file(in_session: sqlalchemy.orm.session, filename: str, b_write_to_console: bool = False):
     """
-        Utility function which stores a database in a local file.
+    Writes all ORM Objects stored within the given session into a local file.
 
-        :param in_session: (sqlalchemy.orm.session) Database Session which should be logged
-        :param filename: (String) Filename of the logfile
-        :param b_write_to_console: (bool) enables writing to the console. Default false
-        """
-    f = open(filename, "w")
-    for table in Base.__subclasses__():
-        for column_object in in_session.query(table).all():
-            if b_write_to_console:
-                print("I am writing: {}".format(str(column_object.__dict__)))
-            f.write(str(column_object.__dict__))
-            f.write("\n")
-    f.close()
-
-
-def printDB(in_SessionMaker):
+    :param in_session: (sqlalchemy.orm.session) Database Session which should be logged
+    :param filename: (String) Filename of the logfile
+    :param b_write_to_console: (bool) enables writing to the console. Default false
     """
-        Prints all ORM Class data within the given Session.
+    with open(filename, "w") as f:
+        for table in Base.__subclasses__():
+            for column_object in in_session.query(table).all():
+                if b_write_to_console:
+                    rospy.loginfo("I am writing: {}".format(str(column_object.__dict__)))
+                f.write(str(column_object.__dict__))
+                f.write("\n")
 
-        :param in_session: (sqlalchemy.orm.session) Database Session which should be printed
-        """
-    memory_session = in_SessionMaker()
+
+def print_database(in_Sessionmaker: sqlalchemy.orm.sessionmaker):
+    """
+    Prints all ORM Class data within the given Session.
+
+    :param in_sessionmaker: (sqlalchemy.orm.session) Database Session which should be printed
+    """
+    memory_session = in_Sessionmaker()
     for table in Base.__subclasses__():
         smt = sqlalchemy.select('*').select_from(table)
         result = memory_session.execute(smt).all()
-        print(result)
+        rospy.loginfo(result)
 
 
-def update_primary_key(source_engine, destination_engine):
-    '''
-    Primary keys of destination will be updated so that there are no collisions when merging.
-    Care it is expected that the database takes care of cascading the change through its foreign keys
-    Will create own session
+def update_primary_key(source_engine: sqlalchemy.orm.engine, destination_engine: sqlalchemy.orm.engine):
+    """
+    Updates all the primary keys of the database associated with the destination engine, so that there will be no
+    problems when merging it into the source database. In order to achieve this the highest id value of the source
+    engine is searched and the primary keys of the destination database will get all the values following that.
+    Cascading triggers in the database will take care of the rest. Careful as today (20.09.2023) this will not work in
+    memory databases as there are no triggers
 
     :param source_engine: (sqlalchemy.orm.engine) Engine of the source data_base
     :param destination_engine: (sqlalchemy.orm.engine) Engine of the destination data_base
-    '''
+    """
     real_session = sqlalchemy.orm.Session(bind=destination_engine)
     memory_session = sqlalchemy.orm.Session(bind=source_engine)
-    primaryKeys = {}
+    primary_keys = {}
     for table in Base.__subclasses__():  # iterate over all tables
-        highest_free_keyValue = 0
-        primaryKeys[table] = {}
+        highest_free_key_value = 0
+        primary_keys[table] = {}
         list_of_primary_keys_of_this_table = table.__table__.primary_key.columns.values()
         for key in list_of_primary_keys_of_this_table:
             # make it smart but maybe
             all_memory_key_values = memory_session.query(key).all()
-            primaryKeys[table][key.name] = real_session.query(key).all()
+            primary_keys[table][key.name] = real_session.query(key).all()
             if all_memory_key_values:
-                highest_free_keyValue = max(all_memory_key_values)[0] + 1  # ToDo: Make it even more generic? # We will add 1 to the id then let the DB to the rest, afterwards we will compy all info over
+                highest_free_key_value = max(all_memory_key_values)[
+                                             0] + 1
+                """"""
+                # ToDo: Make it even more generic? # We will add 1 to the id then let the DB to the rest, afterwards we will compy all info over
             for column_object in real_session.query(table).all():  # iterate over all columns
                 if column_object.__dict__[key.name] in all_memory_key_values:
-                    print("Found primarykey collision in table {} value: {} max value in memory {}".format(table,
-                                                                                                           column_object.__dict__[
-                                                                                                               key.name],
-                                                                                                           highest_free_keyValue))
-                    column_object.__dict__[key.name] = highest_free_keyValue
-                    highest_free_keyValue += 1
+                    rospy.loginfo(
+                        "Found primary_key collision in table {} value: {} max value in memory {}".format(table,
+                                                                                                          column_object.__dict__[
+                                                                                                              key.name],
+                                                                                                          highest_free_key_value))
+                    column_object.__dict__[key.name] = highest_free_key_value
+                    highest_free_key_value += 1
 
     real_session.commit()
     real_session.close()
 
 
-def copy_database(source_session_maker, destination_session_maker):
-    '''
-    Primary keys of destination will be updated so that there are no collisions when merging.
-    Care it is expected that the database takes care of cascading the change through its foreign keys
-    Will create own session
+def copy_database(source_session_maker: sqlalchemy.orm.sessionmaker,
+                  destination_session_maker: sqlalchemy.orm.sessionmaker):
+    """
+    Iterates through all ORM Objects within tht source database and merges them into the destination database. Careful
+    this function does not check if there are any primary key collisions or updates any data.
+
+    note:
+        Ignores all previously detached data, could result in loss of infromation. During testing database objects
+        sometimes had a detached twin. As a possible feature in the future it maybe useful to give the user an
+        opportunity to decide what happens with the detached objects. Careful this could lead to dublicated data in the
+        destination database.
 
     :param source_session_maker: (sqlalchemy.orm.sessionmaker) Sessionmaker of the source database
     :param destination_session_maker: (sqlalchemy.orm.sessionmaker) Sessionmaker of the destination database
-    '''
+    """
     source_session = source_session_maker()
     objects_to_add = []
     try:
@@ -96,11 +108,10 @@ def copy_database(source_session_maker, destination_session_maker):
                         sqlalchemy.orm.make_transient(row[key])
                         objects_to_add.append(row[key])
                     else:
-                        print("WARNING: Ignored already detached ORM Object {} ".format(
-                            row[key]))  # ToDo: Maybe give option to include theses items (could break something)
+                        rospy.logwarn("WARNING: Ignored already detached ORM Object {} ".format(
+                            row[key]))
     except Exception as e:
         traceback.print_exc()
-        # logging.error(traceback.format_exc())
     finally:
         source_session.close()
     if len(objects_to_add) < 1:
