b7bcaacf581d855a29603a33949453c15648bc15
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2023-12-19 17:06:04 +0100

    Re-add ik-solver for backwards compatibility

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -2,6 +2,7 @@
 
 source ${PYCRAM_WS}/devel/setup.bash
 roslaunch rvizweb rvizweb.launch config_file:=/binder/rvizweb-config.json &
+roslaunch pycram ik_and_description.launch &
 
 jupyter lab workspaces import ${PYCRAM_WS}/src/pycram/binder/workspace.json
 
