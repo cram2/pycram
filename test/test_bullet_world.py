02d93e2c45fbdd0ada8781845fdbe44e731851e4
Jonas Dech <jdech@uni-bremen.de>
2024-01-03 14:31:27 +0100

    [tests] moved XML test to test_bullet_world

--- test/test_bullet_world.py
+++ test/test_bullet_world.py
@@ -1,6 +1,12 @@
+import unittest
+
+import rospkg
+
 from bullet_world_testcase import BulletWorldTestCase
 from pycram.pose import Pose
 import numpy as np
+from pycram.bullet_world import fix_missing_inertial
+import xml.etree.ElementTree as ET
 
 
 class BulletWorldTest(BulletWorldTestCase):
@@ -15,3 +21,19 @@ class BulletWorldTest(BulletWorldTestCase):
         #self.robot.set_orientation(list(2*tf.transformations.quaternion_from_euler(0, 0, np.pi, axes="sxyz")))
         self.robot.set_orientation(Pose(orientation=[0, 0, 1, 1]))
         self.assertEqual(self.robot.get_link_position('head_pan_link').z, head_position)
+
+
+class XMLTester(unittest.TestCase):
+
+    def setUp(self) -> None:
+        rospack = rospkg.RosPack()
+        filename = rospack.get_path('pycram') + '/resources/' + 'pr2.urdf'
+        with open(filename, "r") as f:
+            self.urdf_string = f.read()
+
+    def test_inertial(self):
+        result = fix_missing_inertial(self.urdf_string)
+        resulting_tree = ET.ElementTree(ET.fromstring(result))
+        for element in resulting_tree.iter("link"):
+            self.assertTrue(len([*element.iter("inertial")]) > 0)
+
