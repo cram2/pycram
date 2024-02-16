bba4c4fec79134afb3da4ce97fde600b31f45d6b
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2024-02-16 18:10:28 +0100

    [Abstract World] Added more tests for bullet world, Tests are running.

---
+++ test/test_cache_manager.py
@@ -0,0 +1,17 @@
+from bullet_world_testcase import BulletWorldTestCase
+from pycram.enums import ObjectType
+from pycram.urdf_interface import ObjectDescription
+from pycram.world_object import Object
+
+
+class TestCacheManager(BulletWorldTestCase):
+
+    def test_generate_description_and_write_to_cache(self):
+        cache_manager = self.world.cache_manager
+        path = "../resources/apartment.urdf"
+        extension = ".urdf"
+        cache_path = self.world.cache_dir + "apartment.urdf"
+        apartment = Object("apartment", ObjectType.ENVIRONMENT, path, ObjectDescription)
+        cache_manager.generate_description_and_write_to_cache(path, extension, cache_path,
+                                                              apartment.description)
+        self.assertTrue(cache_manager.is_cached(path, apartment.description))
