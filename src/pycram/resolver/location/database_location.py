cc600f6a7be6312240c742b2426ca9a1d5e2020f
David Prüser <dprueser@uni-bremen.de>
2024-03-03 16:55:30 +0100

    [resolver] fixed querying from occupancy costmap II

--- src/pycram/resolver/location/database_location.py
+++ src/pycram/resolver/location/database_location.py
@@ -2,16 +2,14 @@ import numpy as np
 import sqlalchemy.orm
 import sqlalchemy.sql
 from sqlalchemy import select, Select
-from tf import transformations
 import pycram.designators.location_designator
 import pycram.task
 from pycram.costmaps import OccupancyCostmap
 from pycram.orm.action_designator import PickUpAction, Action
 from pycram.orm.object_designator import Object
-from pycram.orm.base import Position, RobotState, Designator, Base
+from pycram.orm.base import Position, RobotState, Pose as ORMPose, Quaternion
 from pycram.orm.task import TaskTreeNode, Code
 from .jpt_location import JPTCostmapLocation
-from ... import orm
 from ...pose import Pose
 
 
@@ -42,26 +40,31 @@ class DatabaseCostmapLocation(pycram.designators.location_designator.CostmapLoca
         """
 
         robot_pos = sqlalchemy.orm.aliased(Position)
+        robot_pose = sqlalchemy.orm.aliased(ORMPose)
+        object_pos = sqlalchemy.orm.aliased(Position)
+        relative_x = robot_pos.x - object_pos.x
+        relative_y = robot_pos.y - object_pos.y
 
         # query all relative robot positions in regard to an objects position
         # make sure to order the joins() correctly
-        query = (select(PickUpAction.arm, PickUpAction.grasp, RobotState.torso_height, robot_pos.x, robot_pos.y)
+        query = (select(PickUpAction.arm, PickUpAction.grasp, RobotState.torso_height, relative_x, relative_y,
+                        Quaternion.x, Quaternion.y, Quaternion.z, Quaternion.w).distinct()
                  .join(TaskTreeNode.code)
                  .join(Code.designator.of_type(PickUpAction))
                  .join(PickUpAction.robot_state)
-                 .join(RobotState.pose)
-                 .join(robot_pos)
-                 .join(PickUpAction.object).where(Object.type == self.target.type)
-                                           .where(TaskTreeNode.status == "SUCCEEDED"))
+                 .join(robot_pose, RobotState.pose)
+                 .join(robot_pos, robot_pose.position)
+                 .join(ORMPose.orientation)
+                 .join(PickUpAction.object)
+                 .join(Object.pose)
+                 .join(object_pos, ORMPose.position).where(Object.type == self.target.type)
+                                                    .where(TaskTreeNode.status == "SUCCEEDED"))
 
         # create Occupancy costmap for the target object
-        position, orientation = self.target.pose.to_list()
-        position = list(position)
-        position[-1] = 0
 
         ocm = OccupancyCostmap(distance_to_obstacle=0.3, from_ros=False, size=200, resolution=0.02,
-                               origin=Pose(position, orientation))
-        # ocm.visualize()
+                               origin=self.target.pose)
+        ocm.visualize()
 
         # working on a copy of the costmap, since found rectangles are deleted
         map = np.copy(ocm.map)
@@ -70,29 +73,22 @@ class DatabaseCostmapLocation(pycram.designators.location_designator.CostmapLoca
 
         filters = []
 
-        # for every index pair (i, j) in the occupancy map
         for i in range(0, map.shape[0]):
             for j in range(0, map.shape[1]):
-
-                # if this index has not been used yet
                 if map[i][j] > 0:
-                    # get consecutive box
-                    width = ocm._find_consectuive_line((i, j), map)
-                    height = ocm._find_max_box_height((i, j), width, map)
+                    curr_width = ocm._find_consectuive_line((i, j), map)
+                    curr_pose = (i, j)
+                    curr_height = ocm._find_max_box_height((i, j), curr_width, map)
 
-                    # mark box as used
-                    map[i:i + height, j:j + width] = 0
+                    x_lower = (curr_pose[0] - origin[0]) * ocm.resolution
+                    x_upper = (curr_pose[0] + curr_width - origin[0]) * ocm.resolution
+                    y_lower = (curr_pose[1] - origin[1]) * ocm.resolution
+                    y_upper = (curr_pose[1] + curr_height - origin[1]) * ocm.resolution
 
-                    # calculate to coordinates relative to the objects pose
-                    pose = np.array([i, j])
-                    lower_corner = (pose - origin) * ocm.resolution
-                    upper_corner = (pose - origin + np.array([height, width])) * ocm.resolution
-                    rectangle = np.array([lower_corner, upper_corner]).T
+                    map[i:i + curr_height, j:j + curr_width] = 0
 
-                    # transform to jpt query
-                    filters.append(sqlalchemy.and_(robot_pos.x >= rectangle[0][0], robot_pos.x < rectangle[0][1],
-                                                   robot_pos.y >= rectangle[1][0], robot_pos.y < rectangle[1][1]))
-                    # query = self.model.bind({"x": list(rectangle[0]), "y": list(rectangle[1])})
+                    filters.append(sqlalchemy.and_(relative_x >= x_lower, relative_x < x_upper,
+                                                   relative_y >= y_lower, relative_y < y_upper))
 
         return query.where(sqlalchemy.or_(*filters))
 
@@ -104,15 +100,17 @@ class DatabaseCostmapLocation(pycram.designators.location_designator.CostmapLoca
         :return: The costmap location
         """
         target_x, target_y, target_z = self.target.pose.position_as_list()
-        pose = [target_x + sample.x, target_y + sample.y, 0]
-        angle = np.arctan2(pose[1] - target_y, pose[0] - target_x) + np.pi
-        orientation = list(transformations.quaternion_from_euler(0, 0, angle, axes="sxyz"))
+        position = [target_x + sample[3], target_y + sample[4], 0]
+        orientation = [sample[5], sample[6], sample[7], sample[8]]
 
-        result = JPTCostmapLocation.Location(Pose(pose, orientation), sample.arm, sample.torso_height, sample.grasp)
+        result = JPTCostmapLocation.Location(Pose(position, orientation), sample.arm, sample.torso_height, sample.grasp)
         return result
 
     def __iter__(self) -> JPTCostmapLocation.Location:
         statement = self.create_query_from_occupancy_costmap().limit(200)
         samples = self.session.execute(statement).all()
-        for sample in samples:
-            yield self.sample_to_location(sample)
+        if samples:
+            for sample in samples:
+                yield self.sample_to_location(sample)
+        else:
+            raise ValueError("No samples found")
