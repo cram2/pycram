e929478610e60e52afeca6f25d2e3bf0b35db995
duc than <than@uni-bremen.de>
2024-03-14 19:12:27 +0100

    add OntologyOwl class using OwlReady2 to fetch object classes by ontology concepts from SOMA DesignatorDescription add onto_concept attr as an owlready2 Thing ~Action auto fetch onto_concept from OntologyOWL

--- src/pycram/designators/action_designator.py
+++ src/pycram/designators/action_designator.py
@@ -11,22 +11,26 @@ from pycram.designators.actions.actions import (ParkArmsActionPerformable, MoveT
                                                 LookAtActionPerformable, DetectActionPerformable, OpenActionPerformable,
                                                 CloseActionPerformable, GraspingActionPerformable,
                                                 ReleaseActionPerformable)
+from owlready2 import Thing
 
+from pycram.ontology import OntologyOWL
 
 class MoveTorsoAction(ActionDesignatorDescription):
     """
     Action Designator for Moving the torso of the robot up and down
     """
 
-    def __init__(self, positions: List[float], resolver=None):
+    def __init__(self, positions: List[float], resolver=None, onto_concept: Thing = None):
         """
         Create a designator description to move the torso of the robot up and down.
 
         :param positions: List of possible positions of the robots torso, possible position is a float of height in metres
         :param resolver: An optional resolver that returns a performable designator for a designator description.
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.positions: List[float] = positions
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.MoveTorso
 
     def ground(self) -> MoveTorsoActionPerformable:
         """
@@ -51,7 +55,7 @@ class SetGripperAction(ActionDesignatorDescription):
     Set the gripper state of the robot
     """
 
-    def __init__(self, grippers: List[str], motions: List[str], resolver=None):
+    def __init__(self, grippers: List[str], motions: List[str], resolver=None, onto_concept: Thing = None):
         """
         Sets the gripper state, the desired state is given with the motion. Motion can either be 'open' or 'close'.
 
@@ -59,9 +63,11 @@ class SetGripperAction(ActionDesignatorDescription):
         :param motions: A list of possible motions
         :param resolver: An alternative resolver that returns a performable designator for a designator description
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.grippers: List[str] = grippers
         self.motions: List[str] = motions
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.SettingGripper
 
     def ground(self) -> SetGripperActionPerformable:
         """
@@ -89,10 +95,12 @@ class ReleaseAction(ActionDesignatorDescription):
     """
 
     def __init__(self, grippers: List[str], object_designator_description: ObjectDesignatorDescription,
-                 resolver=None):
-        super().__init__(resolver)
+                 resolver=None, onto_concept: Thing = None):
+        super().__init__(resolver, onto_concept)
         self.grippers: List[str] = grippers
         self.object_designator_description = object_designator_description
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.Releasing
 
     def ground(self) -> ReleaseActionPerformable:
         return ReleaseActionPerformable(self.grippers[0], self.object_designator_description.ground())
@@ -110,11 +118,13 @@ class GripAction(ActionDesignatorDescription):
     """
 
     def __init__(self, grippers: List[str], object_designator_description: ObjectDesignatorDescription,
-                 efforts: List[float], resolver=None):
-        super().__init__(resolver)
+                 efforts: List[float], resolver=None, onto_concept: Thing = None):
+        super().__init__(resolver, onto_concept)
         self.grippers: List[str] = grippers
         self.object_designator_description: ObjectDesignatorDescription = object_designator_description
         self.efforts: List[float] = efforts
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.Holding
 
     def ground(self) -> GripActionPerformable:
         return GripActionPerformable(self.grippers[0], self.object_designator_description.ground(), self.efforts[0])
@@ -125,15 +135,17 @@ class ParkArmsAction(ActionDesignatorDescription):
     Park the arms of the robot.
     """
 
-    def __init__(self, arms: List[Arms], resolver=None):
+    def __init__(self, arms: List[Arms], resolver=None, onto_concept: Thing = None):
         """
         Moves the arms in the pre-defined parking position. Arms are taken from pycram.enum.Arms
 
         :param arms: A list of possible arms, that could be used
         :param resolver: An optional resolver that returns a performable designator from the designator description
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.arms: List[Arms] = arms
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.ParkingArms
 
     def ground(self) -> ParkArmsActionPerformable:
         """
@@ -150,7 +162,7 @@ class PickUpAction(ActionDesignatorDescription):
     """
 
     def __init__(self, object_designator_description:  Union[ObjectDesignatorDescription, ObjectDesignatorDescription.Object],
-                 arms: List[str], grasps: List[str], resolver=None):
+                 arms: List[str], grasps: List[str], resolver=None, onto_concept: Thing = None):
         """
         Lets the robot pick up an object. The description needs an object designator describing the object that should be
         picked up, an arm that should be used as well as the grasp from which side the object should be picked up.
@@ -160,11 +172,13 @@ class PickUpAction(ActionDesignatorDescription):
         :param grasps: List of possible grasps for the object
         :param resolver: An optional resolver that returns a performable designator with elements from the lists of possible paramter
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.object_designator_description: Union[
             ObjectDesignatorDescription, ObjectDesignatorDescription.Object] = object_designator_description
         self.arms: List[str] = arms
         self.grasps: List[str] = grasps
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.PickingUp
 
     def ground(self) -> PickUpActionPerformable:
         """
@@ -186,7 +200,7 @@ class PlaceAction(ActionDesignatorDescription):
     def __init__(self,
                  object_designator_description: Union[ObjectDesignatorDescription, ObjectDesignatorDescription.Object],
                  target_locations: List[Pose],
-                 arms: List[str], resolver=None):
+                 arms: List[str], resolver=None, onto_concept: Thing = None):
         """
         Create an Action Description to place an object
 
@@ -195,11 +209,13 @@ class PlaceAction(ActionDesignatorDescription):
         :param arms: List of possible arms to use
         :param resolver: Grounding method to resolve this designator
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.object_designator_description: Union[
             ObjectDesignatorDescription, ObjectDesignatorDescription.Object] = object_designator_description
         self.target_locations: List[Pose] = target_locations
         self.arms: List[str] = arms
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.Placing
 
     def ground(self) -> PlaceActionPerformable:
         """
@@ -218,15 +234,17 @@ class NavigateAction(ActionDesignatorDescription):
     Navigates the Robot to a position.
     """
 
-    def __init__(self, target_locations: List[Pose], resolver=None):
+    def __init__(self, target_locations: List[Pose], resolver=None, onto_concept: Thing = None):
         """
         Navigates the robot to a location.
 
         :param target_locations: A list of possible target locations for the navigation.
         :param resolver: An alternative resolver that creates a performable designator from the list of possible parameter
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.target_locations: List[Pose] = target_locations
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.Navigating
 
     def ground(self) -> NavigateActionPerformable:
         """
@@ -245,7 +263,7 @@ class TransportAction(ActionDesignatorDescription):
     def __init__(self,
                  object_designator_description: Union[ObjectDesignatorDescription, ObjectDesignatorDescription.Object],
                  arms: List[str],
-                 target_locations: List[Pose], resolver=None):
+                 target_locations: List[Pose], resolver=None, onto_concept: Thing = None):
         """
         Designator representing a pick and place plan.
 
@@ -254,11 +272,13 @@ class TransportAction(ActionDesignatorDescription):
         :param target_locations: A list of possible target locations for the object to be placed
         :param resolver: An alternative resolver that returns a performable designator for the list of possible parameter
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.object_designator_description: Union[
             ObjectDesignatorDescription, ObjectDesignatorDescription.Object] = object_designator_description
         self.arms: List[str] = arms
         self.target_locations: List[Pose] = target_locations
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.Transporting
 
     def ground(self) -> TransportActionPerformable:
         """
@@ -278,15 +298,17 @@ class LookAtAction(ActionDesignatorDescription):
     Lets the robot look at a position.
     """
 
-    def __init__(self, targets: List[Pose], resolver=None):
+    def __init__(self, targets: List[Pose], resolver=None, onto_concept: Thing = None):
         """
         Moves the head of the robot such that it points towards the given target location.
 
         :param targets: A list of possible locations to look at
         :param resolver: An alternative resolver that returns a performable designator for a list of possible target locations
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.targets: List[Pose] = targets
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.LookingAt
 
     def ground(self) -> LookAtActionPerformable:
         """
@@ -302,15 +324,17 @@ class DetectAction(ActionDesignatorDescription):
     Detects an object that fits the object description and returns an object designator describing the object.
     """
 
-    def __init__(self, object_designator_description: ObjectDesignatorDescription, resolver=None):
+    def __init__(self, object_designator_description: ObjectDesignatorDescription, resolver=None, onto_concept: Thing = None):
         """
         Tries to detect an object in the field of view (FOV) of the robot.
 
         :param object_designator_description: Object designator describing the object
         :param resolver: An alternative resolver
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.object_designator_description: ObjectDesignatorDescription = object_designator_description
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.LookingFor #CheckingObjectPresence
 
     def ground(self) -> DetectActionPerformable:
         """
@@ -328,7 +352,7 @@ class OpenAction(ActionDesignatorDescription):
     Can currently not be used
     """
 
-    def __init__(self, object_designator_description: ObjectPart, arms: List[str], resolver=None):
+    def __init__(self, object_designator_description: ObjectPart, arms: List[str], resolver=None, onto_concept: Thing = None):
         """
         Moves the arm of the robot to open a container.
 
@@ -336,9 +360,11 @@ class OpenAction(ActionDesignatorDescription):
         :param arms: A list of possible arms that should be used
         :param resolver: A alternative resolver that returns a performable designator for the lists of possible parameter.
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.object_designator_description: ObjectPart = object_designator_description
         self.arms: List[str] = arms
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.Opening
 
     def ground(self) -> OpenActionPerformable:
         """
@@ -358,7 +384,7 @@ class CloseAction(ActionDesignatorDescription):
     """
 
     def __init__(self, object_designator_description: ObjectPart, arms: List[str],
-                 resolver=None):
+                 resolver=None, onto_concept: Thing = None):
         """
         Attempts to close an open container
 
@@ -366,9 +392,11 @@ class CloseAction(ActionDesignatorDescription):
         :param arms: A list of possible arms to use
         :param resolver: An alternative resolver that returns a performable designator for the list of possible parameter
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.object_designator_description: ObjectPart = object_designator_description
         self.arms: List[str] = arms
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.Closing
 
     def ground(self) -> CloseActionPerformable:
         """
@@ -386,7 +414,7 @@ class GraspingAction(ActionDesignatorDescription):
     """
 
     def __init__(self, arms: List[str], object_description: Union[ObjectDesignatorDescription, ObjectPart],
-                 resolver: Callable = None):
+                 resolver: Callable = None, onto_concept: Thing = None):
         """
         Will try to grasp the object described by the given description. Grasping is done by moving into a pre grasp
         position 10 cm before the object, opening the gripper, moving to the object and then closing the gripper.
@@ -395,9 +423,11 @@ class GraspingAction(ActionDesignatorDescription):
         :param object_description: Description of the object that should be grasped
         :param resolver: An alternative resolver to get a specified designator from the designator description
         """
-        super().__init__(resolver)
+        super().__init__(resolver, onto_concept)
         self.arms: List[str] = arms
         self.object_description: ObjectDesignatorDescription = object_description
+        if self.onto_concept is None:
+            self.onto_concept = OntologyOWL.onto.Grasping
 
     def ground(self) -> GraspingActionPerformable:
         """
