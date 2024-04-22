e81d8bfe05bc8d5c05366b0e519841d5228f4907
OEM Configuration (temporary user) <hassouna@uni-bremen.de>
2024-04-22 15:57:26 +0200

    [hsrb] changed naming and some description parts

--- src/pycram/process_modules/__init__.py
+++ src/pycram/process_modules/__init__.py
@@ -1,7 +1,7 @@
 from .pr2_process_modules import Pr2Manager
 from .boxy_process_modules import BoxyManager
 from .donbot_process_modules import DonbotManager
-from .hsr_process_modules import HSRBManager
+from .hsrb_process_modules import HSRBManager
 from .default_process_modules import DefaultManager
 from .stretch_process_modules import StretchManager
 
