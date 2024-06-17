821c98e296e818d3f48b9cbc0eab5aab6306146e
Jonas Dech <j.dech@gmx.net>
2024-06-17 14:40:21 +0200

    [utils] Re-Added supress std out for new robot description

--- src/pycram/utils.py
+++ src/pycram/utils.py
@@ -10,6 +10,7 @@ from inspect import isgeneratorfunction
 from typing_extensions import List, Tuple, Callable
 
 import numpy as np
+import os
 
 from .datastructures.pose import Pose
 import math
@@ -114,3 +115,35 @@ def axis_angle_to_quaternion(axis: List, angle: float) -> Tuple:
     w = math.cos(angle / 2)
 
     return (x, y, z, w)
+
+
+class suppress_stdout_stderr(object):
+    """
+    A context manager for doing a "deep suppression" of stdout and stderr in
+    Python, i.e. will suppress all prints, even if the print originates in a
+    compiled C/Fortran sub-function.
+
+    This will not suppress raised exceptions, since exceptions are printed
+    to stderr just before a script exits, and after the context manager has
+    exited (at least, I think that is why it lets exceptions through).
+    Copied from https://stackoverflow.com/questions/11130156/suppress-stdout-stderr-print-from-python-functions
+    """
+
+    def __init__(self):
+        # Open a pair of null files
+        self.null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+        # Save the actual stdout (1) and stderr (2) file descriptors.
+        self.save_fds = [os.dup(1), os.dup(2)]
+
+    def __enter__(self):
+        # Assign the null pointers to stdout and stderr.
+        os.dup2(self.null_fds[0], 1)
+        os.dup2(self.null_fds[1], 2)
+
+    def __exit__(self, *_):
+        # Re-assign the real stdout/stderr back to (1) and (2)
+        os.dup2(self.save_fds[0], 1)
+        os.dup2(self.save_fds[1], 2)
+        # Close all file descriptors
+        for fd in self.null_fds + self.save_fds:
+            os.close(fd)
