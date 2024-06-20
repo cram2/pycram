f18a6e9edd2b91ff5f61004fc475580cb55a714d
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2024-06-20 17:22:27 +0200

    Minor changes

--- src/pycram/designators/specialized_designators/action/dual_arm_pickup_action.py
+++ src/pycram/designators/specialized_designators/action/dual_arm_pickup_action.py
@@ -1,11 +1,14 @@
 from pycram.designators.action_designator import PickUpAction, PickUpActionPerformable
+from pycram.local_transformer import LocalTransformer
 
 
-class DualGraspingAction(PickUpAction):
+class DualArmPickupAction(PickUpAction):
     """
     Specialization version of the PickUpAction designator which uses heuristics to solve for a dual grasping solution.
     """
 
-    def __iter__(self) -> PickUpActionPerformable:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def ground(self) -> PickUpActionPerformable:
         pass
-    
