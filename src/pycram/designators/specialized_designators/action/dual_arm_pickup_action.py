c66ccf52ecdac1cfe7f1d43748d0d520f4ca8efe
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2024-06-25 16:29:21 +0200

    Add init to DualArmPickupAction

--- src/pycram/designators/specialized_designators/action/dual_arm_pickup_action.py
+++ src/pycram/designators/specialized_designators/action/dual_arm_pickup_action.py
@@ -1,5 +1,7 @@
 from pycram.designators.action_designator import PickUpAction, PickUpActionPerformable
 from pycram.local_transformer import LocalTransformer
+from pycram.datastructures.world import World
+from pycram.robot_descriptions import robot_description
 
 
 class DualArmPickupAction(PickUpAction):
@@ -8,7 +10,14 @@ class DualArmPickupAction(PickUpAction):
     """
 
     def __init__(self, *args, **kwargs):
+        self.local_transformer = LocalTransformer()
+        self.robot = World.robot
+        left_gripper_frame = self.robot.get_link_tf_frame(robot_description.get_tool_frame('left'))
+        right_gripper_frame = self.robot.get_link_tf_frame(robot_description.get_tool_frame('right'))
+        self.gripper_list = [left_gripper_frame, right_gripper_frame]
+
         super().__init__(*args, **kwargs)
 
     def ground(self) -> PickUpActionPerformable:
+
         pass
