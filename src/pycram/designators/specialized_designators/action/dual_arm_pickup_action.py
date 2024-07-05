d7fd027013e39f411fe8b2ccd09fa3763e65b864
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2024-07-05 09:17:20 +0200

    Simplify implementation
    Removed some class properties that are unnecessary and
    added more stable implementations for property calls
    of helper classes.

--- src/pycram/designators/specialized_designators/action/dual_arm_pickup_action.py
+++ src/pycram/designators/specialized_designators/action/dual_arm_pickup_action.py
@@ -23,7 +23,8 @@ class DualArmPickupAction(PickUpAction):
                  grasps: List[str], resolver=None,
                  ontology_concept_holders: Optional[List[Thing]] = None):
         """
-        Specialized version of the PickUpAction designator which uses heuristics to solve for a dual pickup solution.
+        Specialized version of the PickUpAction designator which uses heuristics to solve for a dual pickup problem. The
+        designator will choose the arm which is closest to the object that is to be picked up.
         :param object_designator_description: List of object designator which should be picked up
         :param grasps: List of possible grasps which should be used for the pickup
         :param resolver: Optional specialized_designators that returns a performable designator with elements from the
@@ -39,12 +40,9 @@ class DualArmPickupAction(PickUpAction):
         self.object_designator_description: Union[
             ObjectDesignatorDescription, ObjectDesignatorDescription.Object] = object_designator_description
 
-        self.local_transformer = LocalTransformer()
-        self.robot = World.robot
-        left_gripper_frame = self.robot.get_link_tf_frame(robot_description.get_tool_frame('left'))
-        right_gripper_frame = self.robot.get_link_tf_frame(robot_description.get_tool_frame('right'))
+        left_gripper_frame = World.robot.get_link_tf_frame(robot_description.get_tool_frame('left'))
+        right_gripper_frame = World.robot.get_link_tf_frame(robot_description.get_tool_frame('right'))
         self.gripper_list: List[str] = [left_gripper_frame, right_gripper_frame]
-        self.grasps: List[str] = grasps
 
 
     def ground(self) -> PickUpActionPerformable:
@@ -53,11 +51,14 @@ class DualArmPickupAction(PickUpAction):
         else:
             obj_desig = self.object_designator_description.resolve()
 
+        local_transformer = LocalTransformer()
+
         object_frame = obj_desig.world_object.tf_frame
         distances = []
         # Iterate over possible grippers
         for gripper in self.gripper_list:
-            transform: Transform = self.local_transformer.lookup_transform_from_source_to_target_frame(object_frame, gripper)
+            transform: Transform = local_transformer.lookup_transform_from_source_to_target_frame(gripper,
+                                                                                                  object_frame)
             translation: Vector3 = transform.translation
             distance = norm(array([translation.x, translation.y, translation.z]))
             distances.append(distance)
@@ -65,9 +66,9 @@ class DualArmPickupAction(PickUpAction):
         min_index = distances.index(min(distances))
         winner_frame = self.gripper_list[min_index]
 
-        if "l_gripper" in winner_frame:
+        if winner_frame == robot_description.get_tool_frame('left'):
             winner = "left"
-        elif "r_gripper" in winner_frame:
+        elif winner_frame == robot_description.get_tool_frame('right'):
             winner = "right"
         else:
             raise ValueError(f"Could not determine the winner arm for the pickup action. Winner: {winner_frame}")
