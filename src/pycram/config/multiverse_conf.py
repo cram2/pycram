b33a1ec19dde1167ee285069cf11dcaa28e0349b
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2024-08-02 11:50:29 +0200

    [MultiverseAction] Introduced bullet_mode where the multiverse behaves exactly like bullet_world.
    The simulation is always in paused state unless when the simulate or the step functions are called.

--- src/pycram/config/multiverse_conf.py
+++ src/pycram/config/multiverse_conf.py
@@ -7,7 +7,16 @@ SERVER_PORT: str = "7000"
 simulation_time_step: float = 1e-2
 simulation_frequency: int = int(1 / simulation_time_step)
 
+use_bullet_mode: bool = True
+"""
+If True, the simulation will always be in paused state unless the simulate() function is called, this behaves 
+similar to bullet_world which uses the bullet physics engine.
+"""
+
 use_controller: bool = False
+"""
+Only used when use_bullet_mode is False. This turns on the controller for the robot joints.
+"""
 
 job_handling: conf.JobHandling = conf.JobHandling(let_pycram_move_attached_objects=False,
                                                   let_pycram_handle_spawning=False)
