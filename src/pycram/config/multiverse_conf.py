649c31b2e3eea5f39454669d8999b8de340f6f68
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2024-08-02 11:05:07 +0200

    [MultiverseAction] Tests are working after merge with MultiverseController.
    Removed the use_controller argument, and made it global, and can only be set through multiverse_conf.py

--- src/pycram/config/multiverse_conf.py
+++ src/pycram/config/multiverse_conf.py
@@ -4,7 +4,7 @@ HOST: str = "tcp://127.0.0.1"
 SERVER_HOST: str = HOST
 SERVER_PORT: str = "7000"
 
-simulation_time_step: float = 1e-3
+simulation_time_step: float = 1e-2
 simulation_frequency: int = int(1 / simulation_time_step)
 
 use_controller: bool = False
