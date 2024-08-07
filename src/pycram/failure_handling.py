2a6029debfb25a92b61c1471e07643072aeb8003
Vanessa Hassouna <hassouna@uni-bremen.de>
2024-08-07 13:11:07 +0200

    [failure handling]  A subclass of FailureHandling that implements a retry mechanism that works with a Monitor.

--- src/pycram/failure_handling.py
+++ src/pycram/failure_handling.py
@@ -1,5 +1,8 @@
 from .designator import DesignatorDescription
 from .plan_failures import PlanFailure
+from threading import Lock
+from typing import Union
+from .language import Language, Monitor
 
 
 class FailureHandling:
@@ -11,7 +14,7 @@ class FailureHandling:
     to be extended by subclasses that implement specific failure handling behaviors.
     """
 
-    def __init__(self, designator_description: DesignatorDescription):
+    def __init__(self, designator_description: Union[DesignatorDescription, Monitor]):
         """
         Initializes a new instance of the FailureHandling class.
 
@@ -79,5 +82,75 @@ class Retry(FailureHandling):
                     raise e
 
 
+class RetryMonitor(FailureHandling):
+    """
+    A subclass of FailureHandling that implements a retry mechanism that works with a Monitor.
+    This class represents a specific failure handling strategy that allows us to retry a demo that is
+    being monitored, in case that monitoring condition is triggered.
+    Attributes:
+        max_tries (int): The maximum number of attempts to retry the action.
+    Inherits:
+        All attributes and methods from the FailureHandling class.
+    Overrides:
+        perform(): Implements the retry logic.
+    """
 
+    def __init__(self, designator_description: Monitor, max_tries: int = 3):
+        """
+        Initializes a new instance of the Retry class.
+        Args:
+            designator_description (DesignatorDescription): The description or context
+            of the task or process for which the retry mechanism is being set up.
+            max_tries (int, optional): The maximum number of attempts to retry. Defaults to 3.
+        """
+        super().__init__(designator_description)
+        self.max_tries = max_tries
+        self.lock = Lock()
 
+    def perform(self):
+        """
+        Implementation of the retry mechanism.
+        This method attempts to perform the Monitor + plan specified in the designator_description.
+        If the action fails, it is retried up to max_tries times. If all attempts fail,
+        the last exception is raised. In every loop, we need to clear the kill_event, and set all
+        relevant 'interrupted' variables to False, to make sure the Monitor and plan are executed
+        properly again
+        Raises:
+            PlanFailure: If all retry attempts fail.
+        Returns:
+            The state of the execution performed, as well as a flattened list of the results, in the correct order
+        """
+
+        def reset_interrupted(child):
+            if hasattr(child, "interrupted"):
+                child.interrupted = False
+            for sub_child in getattr(child, "children", []):
+                reset_interrupted(sub_child)
+
+        def flatten(result):
+            flattened_list = []
+            for item in result:
+                if isinstance(item, list):
+                    flattened_list.extend(item)
+                elif isinstance(item, tuple):
+                    flattened_list.append(item)
+                else:
+                    flattened_list.append(item)
+            return flattened_list
+
+        status, res = None, None
+        with self.lock:
+            tries = 0
+            while True:
+                self.designator_description.kill_event.clear()
+                self.designator_description.interrupted = False
+                for child in self.designator_description.children:
+                    reset_interrupted(child)
+                try:
+                    status, res = self.designator_description.perform()
+                    break
+                except PlanFailure as e:
+                    tries += 1
+                    if tries >= self.max_tries:
+                        raise e
+        return status, flatten(res)
