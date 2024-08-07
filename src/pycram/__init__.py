7f5b749616ea90352ca862db874c5921d694a296
Jonas Dech <jdech@uni-bremen.de>
2024-08-07 13:57:27 +0200

    [init] Added singal handler for ctrl+c

--- src/pycram/__init__.py
+++ src/pycram/__init__.py
@@ -1,4 +1,18 @@
 import pycram.process_modules
 # from .specialized_designators import *
 
+from .datastructures.world import World
+import signal
 __version__ = "0.0.2"
+
+
+def signal_handler(sig, frame):
+    logging.info('You pressed Ctrl+C!')
+    World.current_world.exit()
+    print("Exiting...")
+    exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+
