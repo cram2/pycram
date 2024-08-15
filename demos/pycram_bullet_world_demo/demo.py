a2557aa6fd4a399c8fc684c34589777722182c1e
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2024-08-15 19:17:38 +0200

    [MultiverseAction] Demo is working.

--- demos/pycram_bullet_world_demo/demo.py
+++ demos/pycram_bullet_world_demo/demo.py
@@ -46,22 +46,22 @@ def move_and_detect(obj_type):
 
 
 with simulated_robot:
-    # ParkArmsAction([Arms.BOTH]).resolve().perform()
-    #
-    # MoveTorsoAction([0.25]).resolve().perform()
-    #
-    # milk_desig = move_and_detect(ObjectType.MILK)
-    #
-    # TransportAction(milk_desig, [Arms.LEFT], [Pose([4.8, 3.55, 0.8])]).resolve().perform()
-    #
-    # cereal_desig = move_and_detect(ObjectType.BREAKFAST_CEREAL)
-    #
-    # TransportAction(cereal_desig, [Arms.RIGHT], [Pose([5.2, 3.4, 0.8], [0, 0, 1, 1])]).resolve().perform()
-    #
-    # bowl_desig = move_and_detect(ObjectType.BOWL)
-    #
-    # TransportAction(bowl_desig, [Arms.LEFT], [Pose([5, 3.3, 0.8], [0, 0, 1, 1])]).resolve().perform()
-    #
+    ParkArmsAction([Arms.BOTH]).resolve().perform()
+
+    MoveTorsoAction([0.25]).resolve().perform()
+
+    milk_desig = move_and_detect(ObjectType.MILK)
+
+    TransportAction(milk_desig, [Arms.LEFT], [Pose([4.8, 3.55, 0.8])]).resolve().perform()
+
+    cereal_desig = move_and_detect(ObjectType.BREAKFAST_CEREAL)
+
+    TransportAction(cereal_desig, [Arms.RIGHT], [Pose([5.2, 3.4, 0.8], [0, 0, 1, 1])]).resolve().perform()
+
+    bowl_desig = move_and_detect(ObjectType.BOWL)
+
+    TransportAction(bowl_desig, [Arms.LEFT], [Pose([5, 3.3, 0.8], [0, 0, 1, 1])]).resolve().perform()
+
     # Finding and navigating to the drawer holding the spoon
     handle_desig = ObjectPart(names=["handle_cab10_t"], part_of=apartment_desig.resolve())
     drawer_open_loc = AccessingLocation(handle_desig=handle_desig.resolve(),
