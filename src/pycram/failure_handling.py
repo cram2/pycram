0f9291f457829ed604d2ba0d53865afc9757eb02
Luca Krohm <luc_kro@uni-bremen.de>
2024-08-15 04:20:16 +0200

    [RetryMonitor] updated RetryMonitor to the most up to date version, and added test cases

--- src/pycram/failure_handling.py
+++ src/pycram/failure_handling.py
@@ -5,7 +5,7 @@ from typing import Union
 from .language import Language, Monitor
 
 
-class FailureHandling:
+class FailureHandling(Language):
     """
     Base class for failure handling mechanisms in automated systems or workflows.
 
@@ -85,38 +85,60 @@ class Retry(FailureHandling):
 class RetryMonitor(FailureHandling):
     """
     A subclass of FailureHandling that implements a retry mechanism that works with a Monitor.
+
     This class represents a specific failure handling strategy that allows us to retry a demo that is
     being monitored, in case that monitoring condition is triggered.
+
     Attributes:
         max_tries (int): The maximum number of attempts to retry the action.
+        recovery (dict): A dictionary that maps exception types to recovery actions
+
     Inherits:
         All attributes and methods from the FailureHandling class.
+
     Overrides:
         perform(): Implements the retry logic.
     """
 
-    def __init__(self, designator_description: Monitor, max_tries: int = 3):
+    def __init__(self, designator_description: Monitor, max_tries: int = 3, recovery: dict = None):
         """
         Initializes a new instance of the Retry class.
+
         Args:
             designator_description (DesignatorDescription): The description or context
             of the task or process for which the retry mechanism is being set up.
             max_tries (int, optional): The maximum number of attempts to retry. Defaults to 3.
+            recovery (dict, optional): A dictionary that maps exception types to recovery actions. Defaults to None.
         """
         super().__init__(designator_description)
         self.max_tries = max_tries
         self.lock = Lock()
+        if recovery is None:
+            self.recovery = {}
+        else:
+            if not isinstance(recovery, dict):
+                raise ValueError(
+                    "Recovery must be a dictionary with exception types as keys and Language instances as values.")
+            for key, value in recovery.items():
+                if not issubclass(key, BaseException):
+                    raise TypeError("Keys in the recovery dictionary must be exception types.")
+                if not isinstance(value, Language):
+                    raise TypeError("Values in the recovery dictionary must be instances of the Language class.")
+            self.recovery = recovery
 
     def perform(self):
         """
         Implementation of the retry mechanism.
+
         This method attempts to perform the Monitor + plan specified in the designator_description.
         If the action fails, it is retried up to max_tries times. If all attempts fail,
         the last exception is raised. In every loop, we need to clear the kill_event, and set all
         relevant 'interrupted' variables to False, to make sure the Monitor and plan are executed
         properly again
+
         Raises:
             PlanFailure: If all retry attempts fail.
+
         Returns:
             The state of the execution performed, as well as a flattened list of the results, in the correct order
         """
@@ -132,8 +154,6 @@ class RetryMonitor(FailureHandling):
             for item in result:
                 if isinstance(item, list):
                     flattened_list.extend(item)
-                elif isinstance(item, tuple):
-                    flattened_list.append(item)
                 else:
                     flattened_list.append(item)
             return flattened_list
@@ -153,4 +173,7 @@ class RetryMonitor(FailureHandling):
                     tries += 1
                     if tries >= self.max_tries:
                         raise e
+                    exception_type = type(e)
+                    if exception_type in self.recovery:
+                        self.recovery[exception_type].perform()
         return status, flatten(res)
