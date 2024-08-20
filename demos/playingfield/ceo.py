2f113ff1ce628534634c97d5acd9537b70589f05
Nils Leusmann <leusmann@uni-bremen.de>
2024-08-20 15:25:34 +0200

    Kind of working demo

--- demos/playingfield/ceo.py
+++ demos/playingfield/ceo.py
@@ -1,9 +1,18 @@
 from pycram import World
+from pycram.plan_failures import PlanFailure
+from pycram.tasktree import task_tree, reset_tree
 from sandcastle import param_plan
 import traceback
 import hashlib
+import sqlalchemy
+import pycram.orm.base
+
 # extension = ObjectDescription.get_file_extension()
 # Many Plans in many environments with many robots
+# Don't know where to store NEEM so I start with the memory db
+# engine = sqlalchemy.create_engine("sqlite+pysqlite:///:memory:", echo=False)
+# session_maker = sqlalchemy.orm.sessionmaker(bind=engine)
+# session = session_maker()
 # ToDo: Many Plans missing, but this needs function pointers and I am not comfortable with it
 # plans=[]
 robots =[]
@@ -12,23 +21,26 @@ robot_name="pr2.urdf"
 environment_name="house_2.urdf"
 robots.append(robot_name)
 environments.append(environment_name)
-environments.append("apartment.urdf")
 environments.append("Murphys_Law")
+environments.append("apartment.urdf")
+# pycram.orm.base.Base.metadata.create_all(engine)
+# session.commit()
 
 
+v=0
 works=0
 fails=0
 for robot in robots:
     for environment in environments:
+        v+=1
         print("Trying plan: {} with robot: {} in: {}".format("param_plan", robot, environment))
         try:
             param_plan(robot, environment)
             works+=1
             print("Successfully!\n Overall successful tries: {}".format(works))
 
-        except Exception as e:
+        except PlanFailure as e:
             traceback.print_exc()
-            hash(e) # Random Error Value (for now)
             fails+=1
             print("Fail!\n Overall failed tries: {}".format(fails))
         finally:
--- demos/playingfield/sandcastle.py
+++ demos/playingfield/sandcastle.py
@@ -12,7 +12,7 @@ from pycram.datastructures.dataclasses import Color
 def param_plan(robot_path,environment_path):
     world = BulletWorld(WorldMode.GUI)
     robot = Object("pr2", ObjectType.ROBOT, robot_path, pose=Pose([1, 2, 0]))
-    apartment = Object("apartment", ObjectType.ENVIRONMENT, environment_path)
+    apartment = Object(environment_path[:environment_path.find(".")], ObjectType.ENVIRONMENT, environment_path)
 
     milk = Object("milk", ObjectType.MILK, "milk.stl", pose=Pose([1, -1.78, 0.55]),
                   color=Color(1, 0, 0, 1))
@@ -42,47 +42,5 @@ def param_plan(robot_path,environment_path):
         milk_desig = move_and_detect(ObjectType.MILK)
 
         TransportAction(milk_desig, [Arms.LEFT], [Pose([4.8, 3.55, 0.8])]).resolve().perform()
-
-        # cereal_desig = move_and_detect(ObjectType.BREAKFAST_CEREAL)
-        #
-        # TransportAction(cereal_desig, [Arms.RIGHT], [Pose([5.2, 3.4, 0.8], [0, 0, 1, 1])]).resolve().perform()
-        #
-        # bowl_desig = move_and_detect(ObjectType.BOWL)
-        #
-        # TransportAction(bowl_desig, [Arms.LEFT], [Pose([5, 3.3, 0.8], [0, 0, 1, 1])]).resolve().perform()
-        #
-        # # Finding and navigating to the drawer holding the spoon
-        # handle_desig = ObjectPart(names=["handle_cab10_t"], part_of=apartment_desig.resolve())
-        # drawer_open_loc = AccessingLocation(handle_desig=handle_desig.resolve(),
-        #                                     robot_desig=robot_desig.resolve()).resolve()
-        #
-        # NavigateAction([drawer_open_loc.pose]).resolve().perform()
-        #
-        # OpenAction(object_designator_description=handle_desig, arms=[drawer_open_loc.arms[0]]).resolve().perform()
-        # spoon.detach(apartment)
-        #
-        # # Detect and pickup the spoon
-        # LookAtAction([apartment.get_link_pose("handle_cab10_t")]).resolve().perform()
-        #
-        # spoon_desig = DetectAction(BelieveObject(types=[ObjectType.SPOON])).resolve().perform()
-        #
-        # pickup_arm = Arms.LEFT if drawer_open_loc.arms[0] == Arms.RIGHT else Arms.RIGHT
-        # PickUpAction(spoon_desig, [pickup_arm], [Grasp.TOP]).resolve().perform()
-        #
-        # ParkArmsAction([Arms.LEFT if pickup_arm == Arms.LEFT else Arms.RIGHT]).resolve().perform()
-        #
-        # CloseAction(object_designator_description=handle_desig, arms=[drawer_open_loc.arms[0]]).resolve().perform()
-
-        # ParkArmsAction([Arms.BOTH]).resolve().perform()
-        #
-        # MoveTorsoAction([0.15]).resolve().perform()
-        #
-        # # Find a pose to place the spoon, move and then place it
-        # spoon_target_pose = Pose([4.85, 3.3, 0.8], [0, 0, 1, 1])
-        # placing_loc = CostmapLocation(target=spoon_target_pose, reachable_for=robot_desig.resolve()).resolve()
-        #
-        # NavigateAction([placing_loc.pose]).resolve().perform()
-        #
-        # PlaceAction(spoon_desig, [spoon_target_pose], [pickup_arm]).resolve().perform()
 
         ParkArmsAction([Arms.BOTH]).resolve().perform()
