e5c1e1ad02db2c28d203bc1567e288ef61acfd0f
kecks <pkehr@uni-bremen.de>
2024-09-02 15:41:04 +0200

    [vib-demos] added structure for pr2 navigate example

---
+++ demos/pycram_virtual_building_demos/pr2/navigate_demo/src/navigate_demo.py
@@ -0,0 +1,50 @@
+import os
+
+from IPython.core.display_functions import display
+from ipywidgets import Image, HTML
+from pathlib import Path
+
+from demos.pycram_virtual_building_demos.setup.launch_robot import launch_pr2
+from demos.pycram_virtual_building_demos.setup.utils import display_loading_gif
+from pycram.ros.viz_marker_publisher import VizMarkerPublisher
+from pycram.worlds.bullet_world import BulletWorld
+from pycram.designators.action_designator import *
+from pycram.designators.location_designator import *
+from pycram.designators.object_designator import *
+from pycram.datastructures.enums import ObjectType, WorldMode
+from pycram.datastructures.pose import Pose
+from pycram.process_module import simulated_robot, with_simulated_robot
+from pycram.object_descriptors.urdf import ObjectDescription
+from pycram.world_concepts.world_object import Object
+
+
+def navigate_main():
+    extension = ObjectDescription.get_file_extension()
+
+    world = BulletWorld(WorldMode.DIRECT)
+    viz_marker = VizMarkerPublisher()
+    robot = Object("pr2", ObjectType.ROBOT, f"pr2{extension}", pose=Pose([1, 2, 0]))
+    apartment = Object("apartment", ObjectType.ENVIRONMENT, f"apartment{extension}")
+
+    robot_desig = BelieveObject(names=["pr2"])
+    apartment_desig = BelieveObject(names=["apartment"])
+
+    navigate_pose = Pose([2.7, 2.15, 1])
+
+    with simulated_robot:
+        ParkArmsAction([Arms.BOTH]).resolve().perform()
+
+        MoveTorsoAction([0.25]).resolve().perform()
+
+        NavigateAction([navigate_pose]).resolve().perform()
+
+
+def navigate_demo(s=None, w=None, f=None):
+    # launch stuff
+
+    display_loading_gif()
+    # navigate_main()
+
+
+if __name__ == "__main__":
+    navigate_main()
