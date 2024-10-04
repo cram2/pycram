a7f3a5adf72e7ae9d96428d921ff7ad4520f6522
Jonas Dech <j.dech@gmx.net>
2024-10-04 14:31:13 +0200

    [ros] Improved logging with more info

--- src/pycram/ros/logging.py
+++ src/pycram/ros/logging.py
@@ -1,33 +1,64 @@
 import rospy
+import inspect
+from pathlib import Path
+
+
+def _get_caller_method_name():
+    """
+    Get the name of the method that called the function from which this function is called. It is intended as a helper
+    function for the log functions.
+
+    :return: Name of the method that called the function from which this function is called.
+    """
+    return inspect.stack()[2][3]
+
+def _get_caller_method_line():
+    """
+    Get the line of the method that called the function from which this function is called. It is intended as a helper
+    function for the log functions.
+
+    :return: Line numver of the method that called the function from which this function is called.
+    """
+    return inspect.stack()[2][2]
+
+def _get_caller_file_name():
+    """
+    Get the file name of the method that called the function from which this function is called. It is intended as a helper
+    function for the log functions.
+
+    :return: File name of the method that called the function from which this function is called.
+    """
+    path = Path(inspect.stack()[2][1])
+    return path.name
 
 
 def logwarn(message: str):
-    rospy.logwarn(message)
+    rospy.logwarn(f"[{_get_caller_file_name()}:{_get_caller_method_line()}:{_get_caller_method_name()}] {message}")
 
 
 def loginfo(message: str):
-    rospy.loginfo(message)
+    rospy.loginfo(f"[{_get_caller_file_name()}:{_get_caller_method_line()}:{_get_caller_method_name()}] {message}")
 
 
 def logerr(message: str):
-    rospy.logerr(message)
+    rospy.logerr(f"[{_get_caller_file_name()}:{_get_caller_method_line()}:{_get_caller_method_name()}] {message}")
 
 
 def logdebug(message: str):
-    rospy.logdebug(message)
+    rospy.logdebug(f"[{_get_caller_file_name()}:{_get_caller_method_line()}:{_get_caller_method_name()}] {message}")
 
 
 def logwarn_once(message: str):
-    rospy.logwarn_once(message)
+    rospy.logwarn_once(f"[{_get_caller_file_name()}:{_get_caller_method_line()}:{_get_caller_method_name()}] {message}")
 
 
 def loginfo_once(message: str):
-    rospy.loginfo_once(message)
+    rospy.loginfo_once(f"[{_get_caller_file_name()}:{_get_caller_method_line()}:{_get_caller_method_name()}] {message}")
 
 
 def logerr_once(message: str):
-    rospy.logerr_once(message)
+    rospy.logerr_once(f"[{_get_caller_file_name()}:{_get_caller_method_line()}:{_get_caller_method_name()}] {message}")
 
 
 def logdebug_once(message: str):
-    rospy.logdebug_once(message)
+    rospy.logdebug_once(f"[{_get_caller_file_name()}:{_get_caller_method_line()}:{_get_caller_method_name()}] {message}")
