faf1c0e43415f18cfb0372d3554bb561afed6c5f
Jonas Dech <j.dech@gmx.net>
2024-11-26 15:31:32 +0100

    [property] Fixed wrong import

--- src/pycram/datastructures/property.py
+++ src/pycram/datastructures/property.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from abc import abstractmethod
 
-from past.builtins import reduce
+from functools import reduce
 
 from .enums import Arms
 from .dataclasses import ReasoningResult
