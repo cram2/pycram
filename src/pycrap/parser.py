2a1a713d0321876d915d6254b4806fe12788e606
Tom Schierenbeck <tom_sch@uni-bremen.de>
2024-11-29 12:32:27 +0100

    [Ontology] Pre commit before different way of parsing

--- src/pycrap/parser.py
+++ src/pycrap/parser.py
@@ -1,6 +1,10 @@
 import collections
 
+import networkx
+import networkx as nx
 import owlready2
+from graph_tool.topology import topological_sort
+from owlready2 import ThingClass, Thing, ObjectProperty
 from owlready2.base import *
 from owlready2.class_construct import Restriction, SOME, ONLY, VALUE, HAS_SELF, _restriction_type_2_label
 import tqdm
@@ -12,10 +16,8 @@ class Parser:
     A class that parses all definitions from an ontology into python files that are owlready2 compatible.
 
     Not parsed are:
-        - Restrictions
-        - Inverse properties
-        - Functional properties
-        and perhaps something more.
+        - Circular dependencies
+
     """
 
     ontology: owlready2.Ontology
@@ -23,15 +25,31 @@ class Parser:
     The ontology to parse.
     """
 
-    file_name: str
+    path: str
     """
-    The file name to write the parsed ontology to.
+    The path to write the files of the parsed ontology into.
     """
+
+    classes_file_name: str = "classes.py"
+    """
+    The file name where all elements from ontology.classes() are written to.
+    """
+
+    properties_file_name: str = "properties.py"
+    """
+    The file name where all elements from ontology.properties() are written to.
+    """
+
+    restrictions_file_name: str = "restrictions.py"
+
+    individuals_file_name: str = "individuals.py"
 
     indentation = 4
     """
     The indentation to use for the python file.
     """
+
+    dependency_graph: nx.DiGraph
 
     def __init__(self, ontology: owlready2.Ontology, file_name: str):
         self.ontology = ontology
@@ -40,18 +58,79 @@ class Parser:
         render_func_without_namespace = lambda entity: entity.name
         owlready2.set_render_func(render_func_without_namespace)
 
+    def create_dependency_graph(self):
+        """
+        Create the dependency graph of the ontology.
+        """
+        self.dependency_graph = nx.DiGraph()
+        for cls in self.ontology.classes():
+            print(cls)
+            self.dependency_graph.add_node(cls)
+            for other in self.get_concepts_of_elements(cls.is_a):
+                self.dependency_graph.add_edge(other, cls)
+            for other in self.get_concepts_of_elements(cls.equivalent_to):
+                self.dependency_graph.add_edge(other, cls)
+
+        # for prop in owlready2.ObjectProperty.subclasses():
+        #     self.dependency_graph.add_node(prop)
+        #     for other in self.get_concepts_of_elements(prop.is_a):
+        #         self.dependency_graph.add_edge(other, prop)
+        #     for other in self.get_concepts_of_elements(prop.equivalent_to):
+        #         self.dependency_graph.add_edge(other, prop)
+        #     if prop.domain:
+        #         for domain in self.get_concepts_of_elements(prop.domain):
+        #             self.dependency_graph.add_edge(domain, prop)
+        #     if prop.range:
+        #         for range in self.get_concepts_of_elements(prop.range):
+        #             self.dependency_graph.add_edge(range, prop)
+            # if prop.inverse_property:
+            #     for inverse_property in self.get_concepts_of_element(prop.inverse_property):
+            #         self.graph.add_edge(prop, inverse_property)
+
+    def get_concepts_of_elements(self, elements: List) -> List:
+        return [e for element in elements for e in self.get_concepts_of_element(element)]
+
+    def get_concepts_of_element(self, element) -> List:
+        if element is ThingClass:
+            return []
+        elif isinstance(element, ThingClass):
+            return [element]
+        elif isinstance(element, Thing):
+            return [element]
+        elif element.__module__ == 'builtins':
+            return [element]
+        elif isinstance(element, owlready2.prop.DatatypeClass):
+            return [element]
+        elif isinstance(element, normstr):
+            return [element]
+        elif isinstance(element, owlready2.prop.PropertyClass) and not isinstance(element, ObjectProperty):
+            return []
+        elif isinstance(element, owlready2.class_construct.And):
+            return self.get_concepts_of_elements(element.Classes)
+        elif isinstance(element, owlready2.class_construct.Or):
+            return self.get_concepts_of_elements(element.Classes)
+        elif isinstance(element, owlready2.class_construct.Restriction):
+            return self.get_concepts_of_element(element.value)
+        elif isinstance(element, owlready2.class_construct.Not):
+            return self.get_concepts_of_element(element.Class)
+        elif isinstance(element, owlready2.class_construct.OneOf):
+            return self.get_concepts_of_elements(element.instances)
+        elif element.__module__ == "owlready2.util":
+            return []
+        else:
+            raise NotImplementedError(f"Cant get concepts of {element} with type {type(element)}")
 
     def parse(self):
         """
         Parses the ontology into a python file.
         """
+        self.create_dependency_graph()
         self.file = open(self.file_name, "w")
         self.create_imports()
         self.create_namespace()
         self.create_base_class()
         self.create_base_property()
-        self.parse_classes()
-        self.parse_properties()
+        self.create_nodes_and_properties()
         self.file.close()
 
     def create_imports(self):
@@ -68,29 +147,21 @@ class Parser:
         self.file.write("class BaseProperty(ObjectProperty):\n")
         self.file.write("    namespace = default_namespace\n\n")
 
-    def classes_in_bfs(self):
-        visited = []
-        all_classes = collections.deque(owlready2.Thing.subclasses())
-        while all_classes:
-            cls = all_classes.popleft()
-            if cls in visited:
+
+    def create_nodes_and_properties(self):
+
+        # start will all nodes that have no incoming edges
+        assert nx.is_directed_acyclic_graph(self.dependency_graph), "Only DAGs can be parsed for now."
+        for node in nx.topological_sort(self.dependency_graph):
+            if isinstance(node, owlready2.prop.ObjectPropertyClass):
+                self.parse_property(node)
+            elif isinstance(node, owlready2.ThingClass):
+                self.parse_class(node)
+            else:
                 continue
-            all_classes.extend(cls.subclasses())
-            yield cls
-            visited.append(cls)
+                raise NotImplementedError(f"Parsing of node {node} with type {type(node)} is not supported.")
+            self.file.write("\n\n")
 
-    def properties_in_bfs(self):
-        visited = []
-        all_properties = collections.deque(owlready2.ObjectProperty.subclasses())
-        while all_properties:
-            prop = all_properties.popleft()
-
-            if prop in visited:
-                continue
-
-            all_properties.extend(prop.subclasses())
-            yield prop
-            visited.append(prop)
 
     def apply_indent_to(self, string):
         return " " * self.indentation + string.replace('\n', '\n' + ' ' * self.indentation)
@@ -129,7 +200,7 @@ class Parser:
     def write_equivalent_to(self, cls):
         equivalent_to = self.parse_elements(cls.equivalent_to)
         if equivalent_to:
-            equivalent_to = self.apply_indent_to("equivalent_to = [{equivalent_to}]")
+            equivalent_to = self.apply_indent_to(f"equivalent_to = [{equivalent_to}]")
             self.file.write(equivalent_to)
             self.file.write("\n")
 
@@ -144,18 +215,8 @@ class Parser:
         self.file.write(f"class {cls.name}({inherited_classes_sting}):\n")
         self.write_docstring(cls)
         self.file.write("\n")
-        is_a = self.parse_elements(cls.is_a)
-        is_a = f"    is_a = [{is_a}] \n"
-        self.file.write(is_a)
+        self.write_is_a(cls)
         self.write_equivalent_to(cls)
-
-    def parse_classes(self):
-        """
-        Parses all classes from the ontology.
-        """
-        for cls in self.classes_in_bfs():
-            self.parse_class(cls)
-            self.file.write("\n\n")
 
     def parse_property(self, prop):
         self.file.write(f"class {prop.name}(Base):\n")
@@ -181,16 +242,4 @@ class Parser:
         if prop.inverse_property:
             inverse_property = self.apply_indent_to(f"inverse_property = {self.parse_element(prop.inverse_property)}")
             self.file.write(inverse_property)
-            self.file.write("\n")
-
-
-    def parse_properties(self):
-        """
-        Parses all properties from the ontology.
-
-        ..Note:: This does not check for transitively, etc.
-
-        """
-        for prop in self.properties_in_bfs():
-            self.parse_property(prop)
             self.file.write("\n")
--- test/test_pycrap/test_parser.py
+++ test/test_pycrap/test_parser.py
@@ -1,6 +1,9 @@
 import tempfile
 import unittest
 
+import networkx as nx
+from matplotlib import pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout
 from owlready2 import get_ontology
 from pycrap.parser import Parser
 
@@ -11,16 +14,20 @@ class ParserTestCase(unittest.TestCase):
     def setUpClass(cls):
         cls.onto = get_ontology("http://protege.stanford.edu/ontologies/pizza/pizza.owl").load()
         # cls.onto = get_ontology("http://www.ease-crc.org/ont/SOMA.owl").load()
-        cls.namespace = cls.onto.get_namespace("http://www.co-ode.org/ontologies/pizza/pizza.owl#")
+        # cls.namespace = cls.onto.get_namespace("http://www.co-ode.org/ontologies/pizza/pizza.owl#")
         # cls.namespace.Pizza.comment = ["A pizza"]
         # cls.namespace.hasTopping.comment = ["Swaggy topping"]
         cls.file = tempfile.NamedTemporaryFile()
-        cls.parser = Parser(cls.onto, cls.file.name)
+        cls.parser = Parser(cls.onto, "/home/tom_sch/.config/JetBrains/PyCharm2024.3/scratches/pizza.py")# cls.file.name)
 
 
-    def test_parse_restriction(self):
-        concept = self.namespace.Pizza
-
+    def test_create_graph(self):
+        self.parser.create_dependency_graph()
+        pos = graphviz_layout(self.parser.dependency_graph)
+        nx.draw(self.parser.dependency_graph, with_labels=True, pos=pos)
+        plt.show()
+        print(nx.is_directed_acyclic_graph(self.parser.dependency_graph))
+        print(*nx.simple_cycles(self.parser.dependency_graph))
 
     def test_parsing(self):
         self.parser.parse()
