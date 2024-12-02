8b46f1338fc48b0283aec847406b167707faf370
Jonas Dech <j.dech@gmx.net>
2024-12-02 16:24:49 +0100

    [general] Updated Manifest

--- setup.py
+++ setup.py
@@ -1,12 +1,41 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+import os
+if os.environ.get('ROS_VERSION', None) == '1':
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+	## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-	packages=['pycram'],
-	package_dir={'': 'src'}
-)
+	from distutils.core import setup
+	from catkin_pkg.python_setup import generate_distutils_setup
 
-setup(**setup_args)
+	# fetch values from package.xml
+	setup_args = generate_distutils_setup(
+		packages=['pycram'],
+		package_dir={'': 'src'}
+	)
+
+	setup(**setup_args)
+elif os.environ.get('ROS_VERSION', None) == '2':
+	from setuptools import find_packages, setup
+
+	package_name = 'pycram'
+
+	setup(
+		name=package_name,
+		version='0.0.2',
+		packages=find_packages(exclude=['test']),
+		data_files=[
+			('share/ament_index/resource_index/packages',
+			 ['resource/' + package_name]),
+			('share/' + package_name, ['package.xml']),
+		],
+		install_requires=['setuptools'],
+		zip_safe=True,
+		maintainer='Jons Dech',
+		maintainer_email='jdech@uni-bremen.de',
+		description='TODO: Package description',
+		license='GPLv3',
+		tests_require=['pytest'],
+		entry_points={
+			'console_scripts': [
+			],
+		},
+	)
