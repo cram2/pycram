8b471bbc960fc4b9964468eddbe56fdceb1dc274
A_Bassiouny <abdelrhman_bassuny@hotmail.com>
2024-12-09 22:17:24 +0100

    [Logging] Corrected logger name in logdebug.

--- test/test_logging.py
+++ test/test_logging.py
@@ -1,5 +1,5 @@
 from pycram.testing import BulletWorldTestCase
-from pycram.ros.logging import set_logger_level, logwarn, logerr
+from pycram.ros.logging import set_logger_level, logwarn, logerr, logdebug
 from pycram.datastructures.enums import LoggerLevel
 
 
@@ -7,8 +7,11 @@ class TestLogging(BulletWorldTestCase):
     """Testcase for the pycram logging functions."""
 
     def test_set_logger_level(self):
-        logwarn("This is a warning, it should not be printed")
+        set_logger_level(LoggerLevel.DEBUG)
+        logdebug("This is a debug message, it should be printed")
+        logwarn("This is a warning, it should be printed")
         logerr("This is an error, it should be printed")
         set_logger_level(LoggerLevel.ERROR)
+        logdebug("This is a debug message, it should not be printed")
         logwarn("This is a warning, it should not be printed")
         logerr("This is an error, it should be printed")
