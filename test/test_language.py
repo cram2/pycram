8b782e1d4b7a2f9e9647685177d3520f5501399d
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2025-02-11 13:44:21 +0100

    [RayTest] fix wrongly added feature from other pull request.

--- test/test_language.py
+++ test/test_language.py
@@ -224,10 +224,10 @@ class LanguageTestCase(BulletWorldTestCase):
         self.assertEqual(self.robot.get_pose(), Pose([0.3, 0.3, 0]))
         self.assertEqual(self.robot.get_joint_position("torso_lift_joint"), 0.3)
         for joint, pose in RobotDescription.current_robot_description.get_static_joint_chain("right",
-                                                                                             StaticJointState.Park).items():
+                                                                                             "park").items():
             self.assertEqual(self.world.robot.get_joint_position(joint), pose)
         for joint, pose in RobotDescription.current_robot_description.get_static_joint_chain("left",
-                                                                                             StaticJointState.Park).items():
+                                                                                             "park").items():
             self.assertEqual(self.world.robot.get_joint_position(joint), pose)
 
     def test_perform_code(self):
