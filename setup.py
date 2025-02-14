4c63520535ce971528d200d50f0e7de96aa80f60
Jonas Dech <j.dech@gmx.net>
2025-02-14 14:37:42 +0100

    [setup] Changed setup.py for ROS1/2 compatability

--- setup.py
+++ setup.py
@@ -1,38 +1,53 @@
 from setuptools import find_packages, setup
 import os
 
-package_name = 'pycram'
+if os.environ.get('ROS_VERSION') == "1":
+    ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-# Iterate through all the files and subdirectories
-# to build the data files array
-def generate_data_files(share_path, dir):
-    data_files = []
+    from distutils.core import setup
+    from catkin_pkg.python_setup import generate_distutils_setup
 
-    for path, _, files in os.walk(dir):
-        list_entry = (share_path + path, [os.path.join(path, f) for f in files if not f.startswith('.')])
-        data_files.append(list_entry)
+    # fetch values from package.xml
+    setup_args = generate_distutils_setup(
+        packages=['pycram'],
+        package_dir={'': 'src'}
+    )
 
-    return data_files
+    setup(**setup_args)
+elif os.environ.get('ROS_VERSION') == "2":
+
+    package_name = 'pycram'
+
+    # Iterate through all the files and subdirectories
+    # to build the data files array
+    def generate_data_files(share_path, dir):
+        data_files = []
+
+        for path, _, files in os.walk(dir):
+            list_entry = (share_path + path, [os.path.join(path, f) for f in files if not f.startswith('.')])
+            data_files.append(list_entry)
+
+        return data_files
 
 
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ] + generate_data_files('share/' + package_name + '/', 'resources'),
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='jonas',
-    maintainer_email='j.dech@gmx.net',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-        ],
-    },
-)
+    setup(
+        name=package_name,
+        version='0.0.0',
+        packages=find_packages(exclude=['test']),
+        data_files=[
+            ('share/ament_index/resource_index/packages',
+                ['resource/' + package_name]),
+            ('share/' + package_name, ['package.xml']),
+        ] + generate_data_files('share/' + package_name + '/', 'resources'),
+        install_requires=['setuptools'],
+        zip_safe=True,
+        maintainer='jonas',
+        maintainer_email='j.dech@gmx.net',
+        description='TODO: Package description',
+        license='TODO: License declaration',
+        tests_require=['pytest'],
+        entry_points={
+            'console_scripts': [
+            ],
+        },
+    )
