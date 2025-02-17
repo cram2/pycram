20fa1136b91ec45fe9bc96d177f98972febe08a1
Jonas Dech <j.dech@gmx.net>
2025-02-17 20:54:21 +0100

    [world conf] even quicker fix

--- config/world_conf.py
+++ config/world_conf.py
@@ -2,6 +2,7 @@ import math
 import os
 
 import yaml
+import os
 from typing_extensions import Tuple, Type, Dict
 from ..description import ObjectDescription
 from ..object_descriptors.urdf import ObjectDescription as URDF
@@ -14,8 +15,10 @@ class WorldConfig:
     A class to store the configuration of the world, this can be inherited to create a new configuration class for a
     specific world (e.g. multiverse has MultiverseConfig which inherits from this class).
     """
-
-    resources_path = os.path.join(os.path.dirname(__file__), '..', '..', 'resources')
+    if os.environ.get("ROS_VERSION") == "2":
+        resources_path = os.path.join(os.path.dirname(__file__), '..', '..', 'resources')
+    else:
+        resources_path = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'resources')
     resources_path = os.path.abspath(resources_path)
     """
     Global reference for the resources path, this is used to search for the description files of the robot and
