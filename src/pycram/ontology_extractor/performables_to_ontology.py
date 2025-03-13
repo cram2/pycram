41c206d872c3a367075b8a5578f89641c6dcdbf2
Manuel Scheibl <manuel.scheibl@uni-bielefeld.de>
2025-03-13 15:55:00 +0100

    Update

--- src/pycram/ontology_extractor/performables_to_ontology.py
+++ src/pycram/ontology_extractor/performables_to_ontology.py
@@ -1,4 +1,3 @@
-import types
 from enum import EnumMeta
 from pycram.designators.action_designator import ActionAbstract
 from random_events.utils import recursive_subclasses
@@ -16,18 +15,39 @@ class ParameterDigest:
     Encapsulation of meta information about a parameter.
     """
     clazz: Any
+    """
+    Class of the parameter.
+    """
     parameter_name: str
+    """
+    Name of the parameter.
+    """
     docstring_of_parameter_clazz: str
+    """
+    Docstring of the parameter's class.
+    """
     docstring_of_parameter: str
+    """
+    Docstring of the parameter itself (individual to each performable).
+    """
     parameter_default_value: Any
+    """
+    Holds the default value of the parameter if set.
+    """
     is_enum: bool
+    """
+    True if the parameter is an enum. Else False.
+    """
     is_optional: bool
+    """
+    If the parameter type hint is optional, this will be True. Else False.
+    """
 
     def get_default_value(self):
-        if not self.parameter_default_value == inspect.Parameter.empty:
-            return [str(self.parameter_default_value)]
-        else:
+        if self.parameter_default_value == inspect.Parameter.empty:
             return None
+        else:
+            return [str(self.parameter_default_value)]
 
 class ActionAbstractDigest:
     def __init__(self, clazz):
@@ -41,14 +61,15 @@ class ActionAbstractDigest:
     def extract_dataclass_parameter_information(clazz) -> List[ParameterDigest]:
         """
         Extracts information about dataclass parameters from a dataclass.
-        :param clazz: Clazz to parse
+        :param clazz: Class to parse
         :return: List of parameter information.
         """
 
         def is_optional_type(t):
             if get_origin(t) is Union:
                 return type(None) in get_args(t)
-            return False
+            else:
+                return False
 
         with open(inspect.getfile(clazz), 'r') as file:
             file_content = file.read()
@@ -80,7 +101,7 @@ class ActionAbstractDigest:
         ) for param in list(parameters_inspection.keys())]
 
 def create_ontology_from_performables():
-    def unwrap_clazzname(parameter: ParameterDigest) -> str:
+    def unwrap_classname(parameter: ParameterDigest) -> str:
         def extract_content_between_quotes(text: str) -> str:
             if match := re.search(r"'(.*?)'", text):
                 return match.group(1)
@@ -118,7 +139,7 @@ def create_ontology_from_performables():
     all_param_classes_to_ontological_class = {}
     for clazz in classes:
         for param in clazz.parameters:
-            if (clazzname := unwrap_clazzname(param)) not in all_param_classes_to_ontological_class.keys():
+            if (clazzname := unwrap_classname(param)) not in all_param_classes_to_ontological_class.keys():
                 parameter_clazz = types.new_class(clazzname, (Parameter,))
                 parameter_clazz.has_description = param.docstring_of_parameter_clazz
                 if param.is_enum:
@@ -133,7 +154,7 @@ def create_ontology_from_performables():
         performable.has_description = [clazz_digest.docstring]
         params = []
         for param in clazz_digest.parameters:
-            param_instance = all_param_classes_to_ontological_class[unwrap_clazzname(param)](param.parameter_name)
+            param_instance = all_param_classes_to_ontological_class[unwrap_classname(param)](param.parameter_name)
             param_instance.has_description = [param.docstring_of_parameter]
             param_instance.is_optional = [True] if param.is_optional else [False]
             
