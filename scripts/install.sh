f19a0a502444dfade0aa153a353cbf1adf09c9c0
Jonas Dech <j.dech@gmx.net>
2025-04-01 18:00:05 +0200

    [install script] Revamped install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+echo "Setting up workspace structure"
 mkdir -p ~/workspace/ros/src
 cd ~/workspace/ros
-catkin build
-source install/setup.bash
 cd ~/workspace/ros/src
 
+echo "Source ROS installation"
 source /opt/ros/$ROS_DISTRO/setup.bash
 echo "Cloning pycram and dependencies"
 if [ "$ROS_VERSION" = "1" ]; then
@@ -15,15 +15,14 @@ else
 fi
 
 echo "Setting up virtual environment"
-python -m venv pycram/pycram-venv --system-site-packages
+python3 -m venv pycram/pycram-venv --system-site-packages
 source pycram/pycram-venv/bin/activate
 echo "Installing python dependencies"
 pip install -U pip
 pip install -U setuptools
 pip install -r pycram/requirements.txt
 echo "Checking for dependencies of other ros packages"
-rosdep update && rosdep install --from-paths . -i -y
-cd ..
+cd ~/workspace/ros
 echo "Building workspace"
 if [ "$ROS_VERSION" = "1" ]; then
     catkin build
@@ -33,17 +32,4 @@ else
     colcon build --symlink-install
     source install/setup.bash
     echo "source ~/workspace/ros/install/setup.bash" >> ~/.bashrc
-fi
-
-printf 'Should IPython startup script be installed? [y/N]: '
-read answer
-
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    if dpkg -s python3-ipython &>/dev/null; then
-      echo 'Found IPython installation'
-    else
-      echo 'Installing IPython'
-      sudo apt-get install python3-ipython
-    fi
-    cp src/pycram/script/ipython_helper/* ~/.ipython/profile_default/startup/
 fi
