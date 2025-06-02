48cb8e9394f1c94eb54fa39c23ae91156cf91e78
Jonas Dech <j.dech@gmx.net>
2025-06-02 11:13:59 +0200

    [world] Moved Signal handler to World

--- src/pycram/__init__.py
+++ src/pycram/__init__.py
@@ -1,14 +1,1 @@
-
-from .datastructures.world import World
-import signal
 __version__ = "1.0.1"
-
-
-def signal_handler(sig, frame):
-    if World.current_world:
-        World.current_world.exit()
-    print("Exiting...")
-    exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
