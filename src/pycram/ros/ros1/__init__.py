ef9d0246024cf09c7dde1c418fe69ea0ad7ff882
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2025-06-16 16:18:18 +0200

    Skip rospy.init_node()

--- src/pycram/ros/ros1/__init__.py
+++ src/pycram/ros/ros1/__init__.py
@@ -1,10 +1,3 @@
-import rospy
-from .ros_tools import  is_master_online
-
-# Check is for sphinx autoAPI to be able to work in a CI workflow
-if is_master_online():
-    rospy.init_node("pycram")
-
 from .data_types import *
 from .ros_tools import  *
 from .logging import *
